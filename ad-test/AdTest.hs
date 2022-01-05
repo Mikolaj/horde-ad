06273f97d926805f9d09902260535b78a9377bfb
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2022-01-05 08:59:32 +0100

    Rename XOR ad tests similarly as the main XOR tests were recently renamed

--- ad-test/AdTest.hs
+++ ad-test/AdTest.hs
@@ -102,23 +102,23 @@ lossSquared u v =
   let diff = u - v
   in diff * diff
 
-nnLoss :: Num r
+nnXorLoss :: Num r
        => (r -> r)
        -> r -> r -> r -> Domain r
        -> r
-nnLoss factivation x y res vec =
+nnXorLoss factivation x y res vec =
   let r = nnXor factivation x y vec
   in lossSquared r res
 
-setLoss :: Num r
+nnXorLossTotal :: Num r
         => (r -> r)
         -> Domain r
         -> r
-setLoss factivation vec =
-  let n1 = nnLoss factivation 0 0 0 vec
-      n2 = nnLoss factivation 0 1 1 vec
-      n3 = nnLoss factivation 1 0 1 vec
-      n4 = nnLoss factivation 1 1 0 vec
+nnXorLossTotal factivation vec =
+  let n1 = nnXorLoss factivation 0 0 0 vec
+      n2 = nnXorLoss factivation 0 1 1 vec
+      n3 = nnXorLoss factivation 1 0 1 vec
+      n4 = nnXorLoss factivation 1 1 0 vec
       n12 = n1 + n2
       n34 = n3 + n4
   in n12 + n34
@@ -135,19 +135,19 @@ ws2 = let w = [-1.37, 2.28, -0.19] in V.fromList $ w ++ w ++ w
 adXorTests :: TestTree
 adXorTests = testGroup "XOR neural net tests (3 errors are expected)"
   [ testCase "0.1 tanhAct ws 500"
-    $ gradDescShow 0.1 (setLoss tanhAct) ws 500
+    $ gradDescShow 0.1 (nnXorLossTotal tanhAct) ws 500
       @?= ([2.256964,2.255974,-0.6184606,0.943269,0.9431414,-1.2784432,1.805072,-1.9925138,-0.704399],1.20509565e-2)
   , testCase "0.1 tanhAct ws 5000"
-    $ gradDescShow 0.1 (setLoss tanhAct) ws 5000
+    $ gradDescShow 0.1 (nnXorLossTotal tanhAct) ws 5000
       @?= ([2.4474504,2.4467778,-0.8350617,1.3046894,1.3045748,-1.8912042,2.3819275,-2.5550227,-0.8139653],1.8524402e-4)
   , testCase "0.01 tanhAct ws2 50000"
-    $ gradDescShow 0.01 (setLoss tanhAct) ws2 50000
+    $ gradDescShow 0.01 (nnXorLossTotal tanhAct) ws2 50000
       @?= ([-1.9872262,2.576039,0.66793317,-1.7813873,2.2283037,-0.9866766,-2.1694322,2.1973324,2.9272876],2.1781659e-4)
   , testCase "0.01 reluAct ws 5000"
-    $ gradDescShow 0.01 (setLoss reluAct) ws 5000  -- no cookie
+    $ gradDescShow 0.01 (nnXorLossTotal reluAct) ws 5000  -- no cookie
       @?= ([0.18997861,0.14774871,0.25415534,0.28254044,0.21788013,0.22178599,8.981165e-2,-6.05783e-2,0.49060053],1.0)
   , testCase "0.1 reluAct ws2 50000"
-    $ gradDescShow 0.1 (setLoss reluAct) ws2 50000  -- no cookie
+    $ gradDescShow 0.1 (nnXorLossTotal reluAct) ws2 50000  -- no cookie
       @?= ([-1.2425352,2.6025252,0.13252532,-1.5821311,1.7432425,-0.72675747,-1.7345629,1.9154371,-0.42541993],2.0)
   ]
 
