586a790334f1c7d8b78c2a57e10b15a0b030bb59
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2022-01-10 14:38:44 +0100

    Refactor dlet

--- src/AD.hs
+++ src/AD.hs
@@ -73,14 +73,17 @@ newtype DeltaMonad r a = DeltaMonad
   { runDeltaMonad :: State (DeltaState r) a }
   deriving (Monad, Functor, Applicative)
 
-dlet :: Delta r -> DeltaMonad r DeltaId
+-- TODO: when varied benchmarks are available, check if returning v always,
+-- except for @Add@, is faster. Probably @Zero@ and @Var@ appear too rarely
+-- to matter if @Scale@ turns out to require bindings.
+dlet :: Delta r -> DeltaMonad r (Delta r)
 dlet v = DeltaMonad $ do
   i <- gets deltaCounter
   modify $ \s ->
     s { deltaCounter = succ i
       , deltaBindings = v : deltaBindings s
       }
-  return i
+  return $! Var i
 
 data DualDelta r = D r (Delta r)
 
@@ -175,24 +178,24 @@ gradDescSmart f n0 params0 = go n0 params0 0.1 gradient0 value0 0 where
 (*\) :: Num r => DualDelta r -> DualDelta r -> DeltaMonad r (DualDelta r)
 (*\) (D u u') (D v v') = do
   d <- dlet $ Add (Scale v u') (Scale u v')
-  return $! D (u * v) (Var d)
+  return $! D (u * v) d
 
 (+\) :: Num r => DualDelta r -> DualDelta r -> DeltaMonad r (DualDelta r)
 (+\) (D u u') (D v v') = do
   d <- dlet $ Add u' v'
-  return $! D (u + v) (Var d)
+  return $! D (u + v) d
 
 (-\) :: Num r => DualDelta r -> DualDelta r -> DeltaMonad r (DualDelta r)
 (-\) (D u u') (D v v') = do
   d <- dlet $ Add u' (Scale (-1) v')
-  return $! D (u - v) (Var d)
+  return $! D (u - v) d
 
 (**\) :: Floating r
       => DualDelta r -> DualDelta r -> DeltaMonad r (DualDelta r)
 (**\) (D u u') (D v v') = do
   d <- dlet $ Add (Scale (v * (u ** (v - 1))) u')
                   (Scale ((u ** v) * log u) v')
-  return $! D (u ** v) (Var d)
+  return $! D (u ** v) d
 
 scalar :: r -> DualDelta r
 scalar k = D k Zero
@@ -200,18 +203,18 @@ scalar k = D k Zero
 scale :: Num r => r -> DualDelta r -> DeltaMonad r (DualDelta r)
 scale k (D u u') = do
   d <- dlet $ Scale k u'
-  return $! D (k * u) (Var d)
+  return $! D (k * u) d
 
 tanhAct :: Floating r => DualDelta r -> DeltaMonad r (DualDelta r)
 tanhAct (D u u') = do
   let y = tanh u
   d <- dlet $ Scale (1 - y * y) u'
-  return $! D y (Var d)
+  return $! D y d
 
 reluAct :: (Num r, Ord r) => DualDelta r -> DeltaMonad r (DualDelta r)
 reluAct (D u u') = do
   d <- dlet $ Scale (if u > 0 then 1 else 0) u'
-  return $! D (max 0 u) (Var d)
+  return $! D (max 0 u) d
 
 -- | Compute the output of a neuron, without applying activation function,
 -- from trainable inputs in @xs@ and parameters (the bias and weights)
@@ -230,7 +233,7 @@ sumTrainableInputs xs offset vec = do
         in D (acc + u * v) (Add acc' (Add (Scale v u') (Scale u v')))
       D xsum xsum' = V.ifoldl' f bias xs
   d <- dlet xsum'
-  return $! D xsum (Var d)
+  return $! D xsum d
 
 -- | Compute the output of a neuron, without applying activation function,
 -- from constant data in @xs@ and parameters (the bias and weights)
@@ -249,7 +252,7 @@ sumConstantData xs offset vec = do
         in D (acc + r * v) (Add acc' (Scale r v'))
       D xsum xsum' = V.ifoldl' f bias xs
   d <- dlet xsum'
-  return $! D xsum (Var d)
+  return $! D xsum d
 
 lossSquared :: Num r => r -> DualDelta r -> DeltaMonad r (DualDelta r)
 lossSquared targ res = do
