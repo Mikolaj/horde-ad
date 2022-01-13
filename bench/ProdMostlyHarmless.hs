a20dba3738a8eb05821996818b432f6a191e2bc8
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2022-01-13 19:06:44 +0100

    Add a benchmark with selectively-fused prod and our library

--- bench/ProdMostlyHarmless.hs
+++ bench/ProdMostlyHarmless.hs
@@ -55,6 +55,8 @@ main = do
         , bench "vec_grad" $ nf vec_grad_prod vecHalf10e8  -- 11.47s
 -- this already takes 35G, so the worse variants not attempted:
 --        , bench "toList_grad" $ nf toList_grad_prod (take 50000000 allxs)
+        , bench "fusion_vec_func" $ nf fusion_vec_prod vecHalf10e8
+        , bench "fusion_vec_grad" $ nf fusion_vec_grad_prod vecHalf10e8
         ]
     ]
 
@@ -70,7 +72,8 @@ vec_prod_aux vec = do
         -- which works fine there, but costs us some cycles and clarity here,
         -- where there's no gradient descent to manage the vectors.
         let x = D valX (snd vec V.! i)
-        acc *\ x  -- no handwritten gradients; only gradient for * is provided
+        acc *\ x  -- no handwritten gradients; only gradient for * is provided;
+                  -- also, no seletive fusion; all let-bindings are present
   V.ifoldM' f (scalar 1) $ fst vec
 
 vec_grad_prod :: (Eq r, Num r, Data.Vector.Unboxed.Unbox r)
@@ -84,3 +87,25 @@ vec_prod = valueDualDelta vec_prod_aux
 toList_grad_prod :: (Eq r, Num r, Data.Vector.Unboxed.Unbox r)
                  => [r] -> [r]
 toList_grad_prod l = V.toList $ vec_grad_prod $ V.fromList l
+
+-- A version with selective fusion. Just one let stuck at the end
+-- in case the outcome of this functions is used multiple time
+-- by it's consumers.
+
+fusion_vec_prod_aux
+  :: forall m r. (DeltaMonad r m, Num r, Data.Vector.Unboxed.Unbox r)
+   => VecDualDelta r -> m (DualDelta r)
+fusion_vec_prod_aux vec = do
+  let f :: DualDelta r -> Int -> r -> DualDelta r
+      f !acc !i !valX =
+        let x = D valX (snd vec V.! i)
+        in acc * x  -- selective fusion, we know nothing repeats here
+  returnLet $ V.ifoldl' f (scalar 1) $ fst vec
+
+fusion_vec_grad_prod :: (Eq r, Num r, Data.Vector.Unboxed.Unbox r)
+                     => Domain r -> Domain' r
+fusion_vec_grad_prod = fst . df fusion_vec_prod_aux
+
+fusion_vec_prod :: (Num r, Data.Vector.Unboxed.Unbox r)
+                => Domain r -> r
+fusion_vec_prod = valueDualDelta fusion_vec_prod_aux
