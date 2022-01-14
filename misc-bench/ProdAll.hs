8d09267d44865e6de5268399107ccd10900ff2a9
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2022-01-14 10:12:11 +0100

    Change the order of benchmarks in ProdAll mode

--- misc-bench/ProdAll.hs
+++ misc-bench/ProdAll.hs
@@ -10,18 +10,47 @@ import qualified ProdManualTools
 import qualified ProdMostlyHarmlessTools
 
 main :: IO ()
-main =
-  defaultMain
-    [ ProdManualTools.bgroup1000
-    , ProdManualTools.bgroup1e6
-    , ProdManualTools.bgroupHalf1e8
-    , ProdMostlyHarmlessTools.bgroup1000
-    , ProdMostlyHarmlessTools.bgroup1e6
-    , ProdMostlyHarmlessTools.bgroupHalf1e8
-    , ProdAdTools.bgroup1000
-    , ProdAdTools.bgroup1e6
-    , ProdAdTools.bgroupHalf1e8
-    , ProdBackpropTools.bgroup1000
-    , ProdBackpropTools.bgroup1e6
-    , ProdBackpropTools.bgroupHalf1e8
-    ]
+main = defaultMain
+  [ bgroup "1000"
+      [ bgroup "manual"
+          [ ProdManualTools.bgroup1000
+          ]
+      , bgroup "ours"
+          [ ProdMostlyHarmlessTools.bgroup1000
+          ]
+      , bgroup "ad"
+          [ ProdAdTools.bgroup1000
+          ]
+      , bgroup "backprop"
+          [ ProdBackpropTools.bgroup1000
+          ]
+      ]
+  , bgroup "1e6"
+      [ bgroup "manual"
+          [ ProdManualTools.bgroup1e6
+          ]
+      , bgroup "ours"
+          [ ProdMostlyHarmlessTools.bgroup1e6
+          ]
+      , bgroup "ad"
+          [ ProdAdTools.bgroup1e6
+          ]
+      , bgroup "backprop"
+          [ ProdBackpropTools.bgroup1e6
+          ]
+      ]
+  , bgroup "5e7"
+      [ bgroup "manual"
+          [ ProdManualTools.bgroupHalf1e8
+          ]
+      , bgroup "ours"
+          [ ProdMostlyHarmlessTools.bgroupHalf1e8
+          ]
+      , bgroup "ad"
+          [ ProdAdTools.bgroupHalf1e8
+          ]
+      , bgroup "backprop"
+          [ ProdBackpropTools.bgroupHalf1e8
+          ]
+      ]
+  ]
