f73fa4866465390affca2830769a4e7a44bd1726
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2022-02-12 18:21:43 +0100

    Experiment with 0, 1, 2 names throughout the Delta module

--- src/HordeAd/Core/Delta.hs
+++ src/HordeAd/Core/Delta.hs
@@ -94,74 +94,74 @@ data DeltaState r = DeltaState
 -- of the vectors in the result type are given in the first few arguments.
 -- The delta state contains a list of mutually-referencing delta bindings
 -- that are to be evaluated, in the given order, starting with the top-level
--- binding provided in the remaining argument.
-evalBindings :: forall r. (Eq r, Numeric r, Num (Vector r))
+-- binding of a scalar type provided in the remaining argument.
+evalBindings :: (Eq r, Numeric r, Num (Vector r))
              => Int -> Int -> Int -> DeltaState r -> Delta r
              -> ( Vector r
                 , Data.Vector.Vector (Vector r)
                 , Data.Vector.Vector (Matrix r) )
-evalBindings dim dimV dimL st d0 =
+evalBindings dim0 dim1 dim2 st dTopLevel =
   -- This is morally @V.create@ and so totally safe,
   -- but we can't just call @V.create@ thrice, because it would run
   -- the @ST@ action thrice, so we inline and extend @V.create@ here.
   runST $ do
-    (res, resV, resL) <- buildVector st d0
-    r <- V.unsafeFreeze $ VM.slice 0 dim res
-    rV <- V.unsafeFreeze $ VM.slice 0 dimV resV
-    rL <- V.unsafeFreeze $ VM.slice 0 dimL resL
+    (finiteMap0, finiteMap1, finiteMap2) <- buildVectors st dTopLevel
+    v0 <- V.unsafeFreeze $ VM.take dim0 finiteMap0
+    v1 <- V.unsafeFreeze $ VM.take dim1 finiteMap1
+    v2 <- V.unsafeFreeze $ VM.take dim2 finiteMap2
     -- Prevent a crash if a parameter not updated.
     let convertMatrix (MatrixOuter Nothing Nothing Nothing) = fromRows []
-        convertMatrix o = convertMatrixOuter o
-    return (r, rV, V.map convertMatrix rL)
+        convertMatrix m = convertMatrixOuter m
+    return (v0, v1, V.map convertMatrix v2)
 
-buildVector :: forall s r. (Eq r, Numeric r, Num (Vector r))
-            => DeltaState r -> Delta r
-            -> ST s ( Data.Vector.Storable.Mutable.MVector s r
-                    , Data.Vector.Mutable.MVector s (Vector r)
-                    , Data.Vector.Mutable.MVector s (MatrixOuter r) )
-buildVector st d0 = do
+buildVectors :: forall s r. (Eq r, Numeric r, Num (Vector r))
+             => DeltaState r -> Delta r
+             -> ST s ( Data.Vector.Storable.Mutable.MVector s r
+                     , Data.Vector.Mutable.MVector s (Vector r)
+                     , Data.Vector.Mutable.MVector s (MatrixOuter r) )
+buildVectors st dTopLevel = do
   let DeltaId counter0 = deltaCounter0 st
       DeltaId counter1 = deltaCounter1 st
       DeltaId counter2 = deltaCounter2 st
-  store <- VM.replicate counter0 0  -- correct value
-  storeV <- VM.replicate counter1 (V.empty :: Vector r)  -- dummy value
-  storeL <- VM.replicate counter2 (MatrixOuter Nothing Nothing Nothing
+  store0 <- VM.replicate counter0 0  -- correct value
+  store1 <- VM.replicate counter1 (V.empty :: Vector r)  -- dummy value
+  store2 <- VM.replicate counter2 (MatrixOuter Nothing Nothing Nothing
                                    :: MatrixOuter r)  -- dummy value
   let addToVector :: Vector r -> Vector r -> Vector r
       addToVector r = \v -> if V.null v then r else v + r
       addToMatrix :: MatrixOuter r -> MatrixOuter r -> MatrixOuter r
       addToMatrix r = \v -> if nullMatrixOuter v then r else plusMatrixOuter v r
-      eval :: r -> Delta r -> ST s ()
-      eval !r = \case
+      eval0 :: r -> Delta r -> ST s ()
+      eval0 !r = \case
         Zero -> return ()
-        Scale k d -> eval (k * r) d
-        Add d1 d2 -> eval r d1 >> eval r d2
-        Var (DeltaId i) -> VM.modify store (+ r) i
-        Dot1 vr vd -> evalV (Numeric.LinearAlgebra.scale r vr) vd
-        SumElements1 vd n -> evalV (konst r n) vd
-        Index1 d i k -> evalV (konst 0 k V.// [(i, r)]) d
+        Scale k d -> eval0 (k * r) d
+        Add d e -> eval0 r d >> eval0 r e
+        Var (DeltaId i) -> VM.modify store0 (+ r) i
+        Dot1 vr vd -> eval1 (Numeric.LinearAlgebra.scale r vr) vd
+        SumElements1 vd n -> eval1 (konst r n) vd
+        Index1 d i k -> eval1 (konst 0 k V.// [(i, r)]) d
 
         -- Most of the impossible cases will be ruled out by GADT
         -- once the conflation fo polymorphisms is cleared.
-        Konst1{} -> error "buildVector: Konst1 can't result in a scalar"
-        Seq1{} -> error "buildVector: Seq1 can't result in a scalar"
-        Append1{} -> error "buildVector: Append1 can't result in a scalar"
-        Slice1{} -> error "buildVector: Slice1 can't result in a scalar"
-        Dot2{} -> error "buildVector: Dot2 can't result in a scalar"
-        DotRow2{} -> error "buildVector: DotRow2 can't result in a scalar"
-        AsRow2{} -> error "buildVector: AsRow2 can't result in a scalar"
-        Seq2{} -> error "buildVector: Seq2 can't result in a scalar"
-      evalV :: Vector r -> Delta (Vector r) -> ST s ()
-      evalV !r = \case
+        Konst1{} -> error "buildVectors: Konst1 can't result in a scalar"
+        Seq1{} -> error "buildVectors: Seq1 can't result in a scalar"
+        Append1{} -> error "buildVectors: Append1 can't result in a scalar"
+        Slice1{} -> error "buildVectors: Slice1 can't result in a scalar"
+        Dot2{} -> error "buildVectors: Dot2 can't result in a scalar"
+        DotRow2{} -> error "buildVectors: DotRow2 can't result in a scalar"
+        AsRow2{} -> error "buildVectors: AsRow2 can't result in a scalar"
+        Seq2{} -> error "buildVectors: Seq2 can't result in a scalar"
+      eval1 :: Vector r -> Delta (Vector r) -> ST s ()
+      eval1 !r = \case
         Zero -> return ()
-        Scale k d -> evalV (k * r) d
-        Add d1 d2 -> evalV r d1 >> evalV r d2
-        Var (DeltaId i) -> VM.modify storeV (addToVector r) i
-        Konst1 d -> V.mapM_ (`eval` d) r
-        Seq1 vd -> V.imapM_ (\i d -> eval (r V.! i) d) vd
-        Append1 d1 k d2 -> evalV (V.take k r) d1 >> evalV (V.drop k r) d2
-        Slice1 i n d k -> evalV (konst 0 i V.++ r V.++ konst 0 (k - i - n)) d
-        Dot2 mr md -> evalL (MatrixOuter (Just mr) (Just r) Nothing) md
+        Scale k d -> eval1 (k * r) d
+        Add d e -> eval1 r d >> eval1 r e
+        Var (DeltaId i) -> VM.modify store1 (addToVector r) i
+        Konst1 d -> V.mapM_ (`eval0` d) r
+        Seq1 vd -> V.imapM_ (\i d -> eval0 (r V.! i) d) vd
+        Append1 d k e -> eval1 (V.take k r) d >> eval1 (V.drop k r) e
+        Slice1 i n d k -> eval1 (konst 0 i V.++ r V.++ konst 0 (k - i - n)) d
+        Dot2 mr md -> eval2 (MatrixOuter (Just mr) (Just r) Nothing) md
           -- this column vector interacted disastrously with @mr = asRow v@
           -- in @(#>!)@, each causing an allocation of a whole new @n^2@ matrix
           -- and then a third with their outer product;
@@ -170,44 +170,46 @@ buildVector st d0 = do
           -- the cost for the manual computation is many extra delta
           -- expressions which, however, with square enough matrices,
           -- don't dominate
-        DotRow2 row md -> evalL (MatrixOuter Nothing (Just r) (Just row)) md
+        DotRow2 row md -> eval2 (MatrixOuter Nothing (Just r) (Just row)) md
           -- this is a way to alleviate the ephemeral matrices problem,
           -- by polluting the API with the detail about the shape
           -- of the passed array (the replicated row shape),
           -- which eliminates two of the three matrix allocations
 
-        Dot1{} -> error "buildVector: unboxed vectors of vectors not possible"
+        Dot1{} -> error "buildVectors: unboxed vectors of vectors not possible"
         SumElements1{} ->
-          error "buildVector: unboxed vectors of vectors not possible"
-        Index1{} -> error "buildVector: unboxed vectors of vectors not possible"
-      evalL :: MatrixOuter r -> Delta (Matrix r) -> ST s ()
-      evalL !r@(MatrixOuter mm mc mr) = \case
+          error "buildVectors: unboxed vectors of vectors not possible"
+        Index1{} ->
+          error "buildVectors: unboxed vectors of vectors not possible"
+      eval2 :: MatrixOuter r -> Delta (Matrix r) -> ST s ()
+      eval2 !r@(MatrixOuter mm mc mr) = \case
         Zero -> return ()
         Scale k d ->
           let !m = maybe k (k *) mm
-          in evalL (MatrixOuter (Just m) mc mr) d
-        Add d1 d2 -> evalL r d1 >> evalL r d2
-        Var (DeltaId i) -> VM.modify storeL (addToMatrix r) i
-        AsRow2 d -> mapM_ (`evalV` d) (toRowsMatrixOuter r)
-        Seq2 md -> zipWithM_ evalV (toRowsMatrixOuter r) (V.toList md)
+          in eval2 (MatrixOuter (Just m) mc mr) d
+        Add d e -> eval2 r d >> eval2 r e
+        Var (DeltaId i) -> VM.modify store2 (addToMatrix r) i
+        AsRow2 d -> mapM_ (`eval1` d) (toRowsMatrixOuter r)
+        Seq2 md -> zipWithM_ eval1 (toRowsMatrixOuter r) (V.toList md)
 
-        Dot1{} -> error "buildVector: unboxed vectors of vectors not possible"
+        Dot1{} -> error "buildVectors: unboxed vectors of vectors not possible"
         SumElements1{} ->
-          error "buildVector: unboxed vectors of vectors not possible"
-        Index1{} -> error "buildVector: unboxed vectors of vectors not possible"
-  eval 1 d0  -- dt is 1 or hardwired in f
+          error "buildVectors: unboxed vectors of vectors not possible"
+        Index1{} ->
+          error "buildVectors: unboxed vectors of vectors not possible"
+  eval0 1 dTopLevel  -- dt is 1 or hardwired in f
   let evalUnlessZero :: DeltaBinding r -> ST s ()
       evalUnlessZero (DScalar (DeltaId i) d) = do
-        r <- store `VM.read` i
+        r <- store0 `VM.read` i
         unless (r == 0) $  -- we init with exactly 0 so the comparison is OK
-          eval r d
+          eval0 r d
       evalUnlessZero (DVector (DeltaId i) d) = do
-        r <- storeV `VM.read` i
+        r <- store1 `VM.read` i
         unless (V.null r) $
-          evalV r d
+          eval1 r d
       evalUnlessZero (DMatrix (DeltaId i) d) = do
-        r <- storeL `VM.read` i
+        r <- store2 `VM.read` i
         unless (nullMatrixOuter r) $
-          evalL r d
+          eval2 r d
   mapM_ evalUnlessZero (deltaBindings st)
-  return (store, storeV, storeL)
+  return (store0, store1, store2)
