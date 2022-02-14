c84ff33702c91b77e6dc614819b1a6aa6a7fbbe9
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2022-02-14 15:30:57 +0100

    Define mini-batches via matrices, not ad hoc gradient descent glue code

--- test/TestMnistRNN.hs
+++ test/TestMnistRNN.hs
@@ -5,7 +5,9 @@ import Prelude
 import           Control.Monad (foldM)
 import           Data.List (foldl', unfoldr)
 import qualified Data.Vector.Generic as V
-import           Numeric.LinearAlgebra (Numeric, Vector, konst, uniformSample)
+import           Numeric.LinearAlgebra
+  (Matrix, Numeric, Vector, konst, uniformSample)
+import qualified Numeric.LinearAlgebra as HM
 import           System.Random
 import           Test.Tasty
 import           Test.Tasty.HUnit hiding (assert)
@@ -366,18 +368,11 @@ fcfcrnnMnistL2 x s@(D u _) variables = do
   (vec2, s2') <- middleLayerMnistRNNL vec1 s2 variables
   return (vec2, append1 s1' s2')
 
-unrollLastG :: forall a m r. (DeltaMonad r m)
-            => (a
-                -> DualNumber (Vector r)
-                -> DualNumberVariables r
-                -> m (DualNumber (Vector r), DualNumber (Vector r)))
-            -> ([a]
-                -> DualNumber (Vector r)
-                -> DualNumberVariables r
-                -> m (DualNumber (Vector r), DualNumber (Vector r)))
+unrollLastG :: forall a b c m r. (DeltaMonad r m)
+            => (a -> b -> DualNumberVariables r -> m (c, b))
+            -> ([a] -> b -> DualNumberVariables r -> m (c, b))
 unrollLastG f xs s0 variables =
-  let g :: (DualNumber (Vector r), DualNumber (Vector r)) -> a
-        -> m (DualNumber (Vector r), DualNumber (Vector r))
+  let g :: (c, b) -> a -> m (c, b)
       g (_, s) x = f x s variables
   in foldM g (undefined, s0) xs
 
@@ -595,21 +590,227 @@ mnistTestCaseRNN prefix epochs maxBatches f ftest flen width nLayers
        let testErrorFinal = 1 - ftest width testData res
        testErrorFinal @?= expected
 
+-- A version written using matrices to express mini-batches of data
+-- and so using matrix multiplication to run the neural net.
+
+hiddenLayerMnistRNNB :: (DeltaMonad r m, Numeric r, Floating (Matrix r))
+                     => Matrix r  -- the mini-batch of data 28x150
+                     -> DualNumber (Matrix r)  -- state for mini-batch 128x150
+                     -> DualNumberVariables r
+                     -> m (DualNumber (Matrix r), DualNumber (Matrix r))
+hiddenLayerMnistRNNB x s variables = do
+  let wX = varL variables 0  -- 128x28
+      wS = varL variables 1  -- 128x128
+      b = varV variables 0  -- 128
+      batchSize = HM.cols x
+      y = wX <>!! x + wS <>! s + asColumn2 b batchSize
+  yTanh <- returnLetL $ tanh y
+  return (yTanh, yTanh)
+
+middleLayerMnistRNNB :: (DeltaMonad r m, Numeric r, Floating (Matrix r))
+                     => DualNumber (Matrix r)  -- 128x150
+                     -> DualNumber (Matrix r)  -- 128x150
+                     -> DualNumberVariables r
+                     -> m (DualNumber (Matrix r), DualNumber (Matrix r))
+middleLayerMnistRNNB batchOfVec@(D u _) s variables = do
+  let wX = varL variables 3  -- 128x128
+      wS = varL variables 4  -- 128x128
+      b = varV variables 2  -- 128
+      batchSize = HM.cols u
+      y = wX <>! batchOfVec + wS <>! s + asColumn2 b batchSize
+  yTanh <- returnLetL $ tanh y
+  return (yTanh, yTanh)
+
+outputLayerMnistRNNB :: (DeltaMonad r m, Numeric r, Num (Matrix r))
+                     => DualNumber (Matrix r)  -- 128x150
+                     -> DualNumberVariables r
+                     -> m (DualNumber (Matrix r))
+outputLayerMnistRNNB batchOfVec@(D u _) variables = do
+  let w = varL variables 2  -- 10x128
+      b = varV variables 1  -- 10
+      batchSize = HM.cols u
+  returnLetL $ w <>! batchOfVec + asColumn2 b batchSize
+
+fcfcrnnMnistB :: (DeltaMonad r m, Numeric r, Floating (Matrix r))
+              => Matrix r
+              -> DualNumber (Matrix r)
+              -> DualNumberVariables r
+              -> m (DualNumber (Matrix r), DualNumber (Matrix r))
+fcfcrnnMnistB = hiddenLayerMnistRNNB
+
+fcfcrnnMnistB2 :: (DeltaMonad r m, Numeric r, Floating (Matrix r))
+               => Matrix r  -- 28x150
+               -> DualNumber (Matrix r)  -- 256x150
+               -> DualNumberVariables r
+               -> m (DualNumber (Matrix r), DualNumber (Matrix r))
+fcfcrnnMnistB2 x s@(D u _) variables = do
+  let len = HM.rows u `div` 2
+      s1 = rowSlice2 0 len s
+      s2 = rowSlice2 len len s
+  (vec1, s1') <- hiddenLayerMnistRNNB x s1 variables
+  (vec2, s2') <- middleLayerMnistRNNB vec1 s2 variables
+  return (vec2, rowAppend2 s1' s2')
+
+zeroStateB :: (DeltaMonad r m, Numeric r)
+           => (Int, Int)
+           -> (a
+               -> DualNumber (Matrix r)
+               -> DualNumberVariables r
+               -> m (DualNumber r2, DualNumber (Matrix r)))
+           -> (a
+               -> DualNumberVariables r
+               -> m (DualNumber r2))
+zeroStateB ij f xs variables =
+  fst <$> f xs (scalar $ konst 0 ij) variables
+
+nnMnistRNNB :: (DeltaMonad r m, Numeric r, Floating (Matrix r))
+            => Int
+            -> [Matrix r]
+            -> DualNumberVariables r
+            -> m (DualNumber (Matrix r))
+nnMnistRNNB width xs variables = do
+  let batchSize = HM.cols $ head xs
+  rnnLayer <- zeroStateB (width, batchSize) (unrollLastG fcfcrnnMnistB)
+                         xs variables
+  outputLayerMnistRNNB rnnLayer variables
+
+nnMnistRNNB2 :: (DeltaMonad r m, Numeric r, Floating (Matrix r))
+             => Int
+             -> [Matrix r]
+             -> DualNumberVariables r
+             -> m (DualNumber (Matrix r))
+nnMnistRNNB2 width xs variables = do
+  let batchSize = HM.cols $ head xs
+  rnnLayer <- zeroStateB (2 * width, batchSize) (unrollLastG fcfcrnnMnistB2)
+                         xs variables
+  outputLayerMnistRNNB rnnLayer variables
+
+nnMnistRNNLossB :: ( DeltaMonad r m, Numeric r, Fractional r
+                   , Num (Vector r), Floating (Matrix r) )
+                => Int
+                -> ([Matrix r], Matrix r)
+                -> DualNumberVariables r
+                -> m (DualNumber r)
+nnMnistRNNLossB width (xs, target) variables = do
+  result <- nnMnistRNNB width xs variables
+  vec@(D u _) <- lossSoftMaxCrossEntropyL target result
+    -- this @asRow@ is safe, because it gets multiplied/subtracted right away
+  returnLet $ scale (recip $ fromIntegral $ V.length u) $ sumElements1 vec
+
+nnMnistRNNLossB2 :: ( DeltaMonad r m, Numeric r, Fractional r
+                    , Num (Vector r), Floating (Matrix r) )
+                 => Int
+                 -> ([Matrix r], Matrix r)
+                 -> DualNumberVariables r
+                 -> m (DualNumber r)
+nnMnistRNNLossB2 width (xs, target) variables = do
+  result <- nnMnistRNNB2 width xs variables
+  vec@(D u _) <- lossSoftMaxCrossEntropyL target result
+  returnLet $ scale (recip $ fromIntegral $ V.length u) $ sumElements1 vec
+
+mnistTestCaseRNNB
+  :: String
+  -> Int
+  -> Int
+  -> (Int
+      -> ([Matrix Double], Matrix Double)
+      -> DualNumberVariables Double
+      -> DeltaMonadGradient Double (DualNumber Double))
+  -> (Int -> [([Vector Double], Vector Double)] -> Domains Double -> Double)
+  -> (Int -> Int -> (Int, [Int], [(Int, Int)]))
+  -> Int
+  -> Int
+  -> Double
+  -> TestTree
+mnistTestCaseRNNB prefix epochs maxBatches f ftest flen width nLayers
+                  expected =
+  let (nParams, lPV, lPL) = flen width nLayers
+      nParamsV = V.fromList lPV
+      nParamsL = V.fromList lPL
+      params0 = V.unfoldrExactN nParams (uniformR (-0.2, 0.2)) $ mkStdGen 44
+      paramsV0 =
+        V.imap (\i nPV -> V.unfoldrExactN nPV (uniformR (-0.2, 0.2))
+                                          (mkStdGen $ 44 + nPV + i))
+               nParamsV
+      paramsL0 = V.imap (\i (rows, cols) ->
+                           uniformSample (44 + rows + i) rows
+                                         (replicate cols (-0.2, 0.2)))
+                        nParamsL
+      totalParams = nParams + V.sum nParamsV
+                    + V.sum (V.map (uncurry (*)) nParamsL)
+      name = prefix ++ " "
+             ++ unwords [ show epochs, show maxBatches
+                        , show width, show nLayers
+                        , show nParams, show (V.length nParamsV)
+                        , show (V.length nParamsL), show totalParams ]
+  in testCase name $ do
+       let rws (input, target) =
+             ( map (\k -> V.slice (k * 28) 28 input) [0 .. 27]
+             , target )
+       trainData <- map rws <$> loadMnistData trainGlyphsPath trainLabelsPath
+       testData <- map rws <$> loadMnistData testGlyphsPath testLabelsPath
+       let packChunk :: [([Vector Double], Vector Double)]
+                     -> ([Matrix Double], Matrix Double)
+           packChunk chunk =
+             let (inputs, outputs) = unzip chunk
+                 behead !acc ([] : _) = reverse acc
+                 behead !acc l = behead (HM.fromColumns (map head l) : acc)
+                                        (map tail l)
+             in (behead [] inputs, HM.fromColumns outputs)
+           -- There is some visual feedback, because some of these take long.
+           runBatch :: (Domains Double, StateAdam Double)
+                    -> (Int, [([Vector Double], Vector Double)])
+                    -> IO (Domains Double, StateAdam Double)
+           runBatch (parameters@(!_, !_, !_), stateAdam) (k, chunk) = do
+             printf "(Batch %d with %d points)\n" k (length chunk)
+             let res@(parameters2, _) =
+                   sgdAdam (f width) (map packChunk $ chunksOf 150 chunk)
+                           parameters stateAdam
+                 trainScore = ftest width chunk parameters2
+                 testScore = ftest width testData parameters2
+             printf "Training error:   %.2f%%\n" ((1 - trainScore) * 100)
+             printf "Validation error: %.2f%%\n" ((1 - testScore ) * 100)
+             return res
+           runEpoch :: Int
+                    -> (Domains Double, StateAdam Double)
+                    -> IO (Domains Double)
+           runEpoch n (params2, _) | n > epochs = return params2
+           runEpoch n paramsStateAdam = do
+             printf "[Epoch %d]\n" n
+             let trainDataShuffled = shuffle (mkStdGen $ n + 5) trainData
+                 chunks = take maxBatches
+                          $ zip [1 ..] $ chunksOf 5000 trainDataShuffled
+             !res <- foldM runBatch paramsStateAdam chunks
+             runEpoch (succ n) res
+       printf "\nEpochs to run/max batches per epoch: %d/%d\n"
+              epochs maxBatches
+       let parameters0 = (params0, paramsV0, paramsL0)
+           stateAdam0 = initialStateAdam parameters0
+       res <- runEpoch 1 (parameters0, stateAdam0)
+       let testErrorFinal = 1 - ftest width testData res
+       testErrorFinal @?= expected
+
 mnistRNNTestsLong :: TestTree
-mnistRNNTestsLong = testGroup "MnistRNN tests long"
+mnistRNNTestsLong = testGroup "MnistRNNlong tests"
   [ mnistTestCaseRNN "99LL 1 epoch, all batches" 1 99
                      nnMnistRNNLossL testMnistRNNL lenMnistRNNL 128 1
                      6.820000000000004e-2
-  , mnistTestCaseRNN "99VV 1 epoch, all batches" 1 99
-                     nnMnistRNNLossV testMnistRNNV lenMnistRNNV 128 1
-                     6.940000000000002e-2
+  , mnistTestCaseRNNB "99BB 1 epoch, all batches" 1 99
+                      nnMnistRNNLossB testMnistRNNL lenMnistRNNL 128 1
+                      6.820000000000004e-2
   , mnistTestCaseRNN "99LL2 1 epoch, all batches" 1 99
                      nnMnistRNNLossL2 testMnistRNNL2 lenMnistRNNL 128 2
                      6.769999999999998e-2
+  , mnistTestCaseRNNB "99BB2 1 epoch, all batches" 1 99
+                      nnMnistRNNLossB2 testMnistRNNL2 lenMnistRNNL 128 2
+                      6.769999999999998e-2
+  , mnistTestCaseRNN "99VV 1 epoch, all batches" 1 99
+                     nnMnistRNNLossV testMnistRNNV lenMnistRNNV 128 1
+                     6.940000000000002e-2
   ]
 
 mnistRNNTestsShort :: TestTree
-mnistRNNTestsShort = testGroup "MnistRNN tests short"
+mnistRNNTestsShort = testGroup "MnistRNNshort tests"
   [ let glyph = V.unfoldrExactN sizeMnistGlyph (uniformR (0, 1))
         label = V.unfoldrExactN sizeMnistLabel (uniformR (0, 1))
         rws v = map (\k -> V.slice (k * 28) 28 v) [0 .. 27]
@@ -630,26 +831,9 @@ mnistRNNTestsShort = testGroup "MnistRNN tests short"
   , mnistTestCaseRNN "1LL 1 epoch, 1 batch" 1 1
                      nnMnistRNNLossL testMnistRNNL lenMnistRNNL 128 1
                      0.35009999999999997
-  , let glyph = V.unfoldrExactN sizeMnistGlyph (uniformR (0, 1))
-        label = V.unfoldrExactN sizeMnistLabel (uniformR (0, 1))
-        rws v = map (\k -> V.slice (k * 28) 28 v) [0 .. 27]
-        trainData = map ((\g -> (rws (glyph g), label g)) . mkStdGen) [1 .. 100]
-    in sgdTestCase "randomVV 100"
-                   (nnMnistRNNLossV 128)
-                   (lenMnistRNNV 128 1)
-                   (return trainData)
-                   58.4107481776972
-  , let rws (input, target) =
-          (map (\k -> V.slice (k * 28) 28 input) [0 .. 27], target)
-    in sgdTestCase "firstVV 100 trainset samples only"
-                   (nnMnistRNNLossV 128)
-                   (lenMnistRNNV 128 1)
-                   (map rws <$> take 100
-                    <$> loadMnistData trainGlyphsPath trainLabelsPath)
-                   2.7348332349644084
-  , mnistTestCaseRNN "1VV 1 epoch, 1 batch" 1 1
-                     nnMnistRNNLossV testMnistRNNV lenMnistRNNV 128 1
-                     0.32889999999999997
+  , mnistTestCaseRNNB "1BB 1 epoch, 1 batch" 1 1
+                      nnMnistRNNLossB testMnistRNNL lenMnistRNNL 128 1
+                      0.35009999999999997
   , let glyph = V.unfoldrExactN sizeMnistGlyph (uniformR (0, 1))
         label = V.unfoldrExactN sizeMnistLabel (uniformR (0, 1))
         rws v = map (\k -> V.slice (k * 28) 28 v) [0 .. 27]
@@ -670,4 +854,27 @@ mnistRNNTestsShort = testGroup "MnistRNN tests short"
   , mnistTestCaseRNN "1LL2 1 epoch, 1 batch" 1 1
                      nnMnistRNNLossL2 testMnistRNNL2 lenMnistRNNL 128 2
                      0.2643
+  , mnistTestCaseRNNB "1BB2 1 epoch, 1 batch" 1 1
+                      nnMnistRNNLossB2 testMnistRNNL2 lenMnistRNNL 128 2
+                      0.2643
+  , let glyph = V.unfoldrExactN sizeMnistGlyph (uniformR (0, 1))
+        label = V.unfoldrExactN sizeMnistLabel (uniformR (0, 1))
+        rws v = map (\k -> V.slice (k * 28) 28 v) [0 .. 27]
+        trainData = map ((\g -> (rws (glyph g), label g)) . mkStdGen) [1 .. 100]
+    in sgdTestCase "randomVV 100"
+                   (nnMnistRNNLossV 128)
+                   (lenMnistRNNV 128 1)
+                   (return trainData)
+                   58.4107481776972
+  , let rws (input, target) =
+          (map (\k -> V.slice (k * 28) 28 input) [0 .. 27], target)
+    in sgdTestCase "firstVV 100 trainset samples only"
+                   (nnMnistRNNLossV 128)
+                   (lenMnistRNNV 128 1)
+                   (map rws . take 100
+                    <$> loadMnistData trainGlyphsPath trainLabelsPath)
+                   2.7348332349644084
+  , mnistTestCaseRNN "1VV 1 epoch, 1 batch" 1 1
+                     nnMnistRNNLossV testMnistRNNV lenMnistRNNV 128 1
+                     0.32889999999999997
   ]
