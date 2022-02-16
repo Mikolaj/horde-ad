a78236a8fd0d23dac687f0a354567cc7a6cf9a8b
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2022-02-16 17:37:06 +0100

    Attempt at the Delta overhaul based on a class

--- src/HordeAd/Core/Delta.hs
+++ src/HordeAd/Core/Delta.hs
@@ -19,7 +19,7 @@
 -- A lot of the remaining additional structure is for introducing
 -- and reducing dimensions.
 module HordeAd.Core.Delta
-  ( Delta (..)
+  ( DeltaScalar (..), DeltaVector (..), DeltaMatrix (..)
   , DeltaId (..)
   , DeltaBinding (..)
   , DeltaState (..)
@@ -41,6 +41,27 @@ import qualified Numeric.LinearAlgebra as HM
 
 import qualified HordeAd.Internal.MatrixOuter as MO
 
+class Delta f where
+  scaleDelta :: r -> f r -> f r
+
+instance Delta DeltaScalar where
+  scaleDelta = ScaleScalar
+
+instance Delta DeltaVector where
+  scaleDelta = ScaleVector
+
+instance Delta DeltaMatrix where
+  scaleDelta = ScaleMatrix
+
+{- then
+
+data DualNumber r = D r (Delta r)
+
+scale :: Num r => r -> DualNumber r -> DualNumber r
+scale r (D u u') = D (r * u) (Scale r u')
+
+-}
+
 -- | This is the grammar of delta-expressions.
 -- They have different but inter-related semantics at the level
 -- of scalars, vectors and matrices (WIP: and arbitrary tensors).
@@ -52,41 +73,51 @@ import qualified HordeAd.Internal.MatrixOuter as MO
 -- In other words, for each choice of the underlying scalar type @r@,
 -- we have three primitive differentiable types based on the scalar:
 -- the scalar type @r@ itself, @Vector r@ and @Matrix r@.
-data Delta :: Type -> Type where
+data DeltaScalar :: Type -> Type where
   -- These constructors make sense at all levels: scalars, vectors, matrices,
   -- tensors. All constructors focusing on scalars belong to this group,
   -- that is, they make sense also for vectors, etc., and have more or less
   -- analogous semantics at the non-scalar levels.
-  Zero :: Delta a
-  Scale :: a -> Delta a -> Delta a
-  Add :: Delta a -> Delta a -> Delta a
-  Var :: DeltaId a -> Delta a
+  ZeroScalar :: DeltaScalar r
+  ScaleScalar :: r -> DeltaScalar r -> DeltaScalar r
+  AddScalar :: DeltaScalar r -> DeltaScalar r -> DeltaScalar r
+  VarScalar :: DeltaId r -> DeltaScalar r
 
-  Dot1 :: Vector r -> Delta (Vector r) -> Delta r
-  SumElements1 :: Delta (Vector r) -> Int -> Delta r
-  Index1 :: Delta (Vector r) -> Int -> Int -> Delta r
+  Dot1 :: Vector r -> DeltaVector r -> DeltaScalar r
+  SumElements1 :: DeltaVector r -> Int -> DeltaScalar r
+  Index1 :: DeltaVector r -> Int -> Int -> DeltaScalar r
 
-  Seq1 :: Data.Vector.Vector (Delta r) -> Delta (Vector r)
-  Konst1 :: Delta r -> Delta (Vector r)
-  Append1 :: Delta (Vector r) -> Int -> Delta (Vector r) -> Delta (Vector r)
-  Slice1 :: Int -> Int -> Delta (Vector r) -> Int -> Delta (Vector r)
-  M_VD2 :: Matrix r -> Delta (Vector r) -> Delta (Vector r)
-  MD_V2 :: Delta (Matrix r) -> Vector r -> Delta (Vector r)
-  SumRows2 :: Delta (Matrix r) -> Int -> Delta (Vector r)
-  SumColumns2 :: Delta (Matrix r) -> Int -> Delta (Vector r)
+data DeltaVector :: Type -> Type where
+  ZeroVector :: DeltaVector r
+  ScaleVector :: Vector r -> DeltaVector r -> DeltaVector r
+  AddVector :: DeltaVector r -> DeltaVector r -> DeltaVector r
+  VarVector :: DeltaId (Vector r) -> DeltaVector r
 
-  Seq2 :: Data.Vector.Vector (Delta (Vector r)) -> Delta (Matrix r)
-  Transpose2 :: Delta (Matrix r) -> Delta (Matrix r)
-  M_MD2 :: Matrix r -> Delta (Matrix r) -> Delta (Matrix r)
-  MD_M2 :: Delta (Matrix r) -> Matrix r -> Delta (Matrix r)
-  AsRow2 :: Delta (Vector r) -> Delta (Matrix r)
-  AsColumn2 :: Delta (Vector r) -> Delta (Matrix r)
-  RowAppend2 :: Delta (Matrix r) -> Int -> Delta (Matrix r) -> Delta (Matrix r)
-  ColumnAppend2
-    :: Delta (Matrix r) -> Int -> Delta (Matrix r) -> Delta (Matrix r)
-  RowSlice2 :: Int -> Int -> Delta (Matrix r) -> Int -> Int -> Delta (Matrix r)
-  ColumnSlice2
-    :: Int -> Int -> Delta (Matrix r) -> Int -> Int -> Delta (Matrix r)
+  Seq1 :: Data.Vector.Vector (DeltaScalar r) -> DeltaVector r
+  Konst1 :: DeltaScalar r -> DeltaVector r
+  Append1 :: DeltaVector r -> Int -> DeltaVector r -> DeltaVector r
+  Slice1 :: Int -> Int -> DeltaVector r -> Int -> DeltaVector r
+  M_VD2 :: Matrix r -> DeltaVector r -> DeltaVector r
+  MD_V2 :: DeltaMatrix r -> Vector r -> DeltaVector r
+  SumRows2 :: DeltaMatrix r -> Int -> DeltaVector r
+  SumColumns2 :: DeltaMatrix r -> Int -> DeltaVector r
+
+data DeltaMatrix :: Type -> Type where
+  ZeroMatrix :: DeltaMatrix r
+  ScaleMatrix :: Matrix r -> DeltaMatrix r -> DeltaMatrix r
+  AddMatrix :: DeltaMatrix r -> DeltaMatrix r -> DeltaMatrix r
+  VarMatrix :: DeltaId (Matrix r) -> DeltaMatrix r
+
+  Seq2 :: Data.Vector.Vector (DeltaVector r) -> DeltaMatrix r
+  Transpose2 :: DeltaMatrix r -> DeltaMatrix r
+  M_MD2 :: Matrix r -> DeltaMatrix r -> DeltaMatrix r
+  MD_M2 :: DeltaMatrix r -> Matrix r -> DeltaMatrix r
+  AsRow2 :: DeltaVector r -> DeltaMatrix r
+  AsColumn2 :: DeltaVector r -> DeltaMatrix r
+  RowAppend2 :: DeltaMatrix r -> Int -> DeltaMatrix r -> DeltaMatrix r
+  ColumnAppend2 :: DeltaMatrix r -> Int -> DeltaMatrix r -> DeltaMatrix r
+  RowSlice2 :: Int -> Int -> DeltaMatrix r -> Int -> Int -> DeltaMatrix r
+  ColumnSlice2 :: Int -> Int -> DeltaMatrix r -> Int -> Int -> DeltaMatrix r
 
 newtype DeltaId a = DeltaId Int
   deriving (Show, Eq)
@@ -95,9 +126,9 @@ newtype DeltaId a = DeltaId Int
 -- but it costs more (they are boxed) than storing them here at the time
 -- of binding creation.
 data DeltaBinding r =
-    DScalar (DeltaId r) (Delta r)
-  | DVector (DeltaId (Vector r)) (Delta (Vector r))
-  | DMatrix (DeltaId (Matrix r)) (Delta (Matrix r))
+    DScalar (DeltaId r) (DeltaScalar r)
+  | DVector (DeltaId (Vector r)) (DeltaVector r)
+  | DMatrix (DeltaId (Matrix r)) (DeltaMatrix r)
 
 data DeltaState r = DeltaState
   { deltaCounter0 :: DeltaId r
@@ -116,7 +147,7 @@ data DeltaState r = DeltaState
 -- that are to be evaluated, in the given order, starting with the top-level
 -- binding of a scalar type provided in the remaining argument.
 evalBindings :: (Eq r, Numeric r, Num (Vector r))
-             => Int -> Int -> Int -> DeltaState r -> Delta r
+             => Int -> Int -> Int -> DeltaState r -> DeltaScalar r
              -> ( Vector r
                 , Data.Vector.Vector (Vector r)
                 , Data.Vector.Vector (Matrix r) )
@@ -134,7 +165,7 @@ evalBindings dim0 dim1 dim2 st dTopLevel =
     return (v0, v1, V.map MO.convertMatrixOuterOrNull v2)
 
 buildVectors :: forall s r. (Eq r, Numeric r, Num (Vector r))
-             => DeltaState r -> Delta r
+             => DeltaState r -> DeltaScalar r
              -> ST s ( Data.Vector.Storable.Mutable.MVector s r
                      , Data.Vector.Mutable.MVector s (Vector r)
                      , Data.Vector.Mutable.MVector s (MO.MatrixOuter r) )
@@ -149,47 +180,23 @@ buildVectors st dTopLevel = do
       addToVector r = \v -> if V.null v then r else v + r
       addToMatrix :: MO.MatrixOuter r -> MO.MatrixOuter r -> MO.MatrixOuter r
       addToMatrix r = \v -> if MO.nullMatrixOuter v then r else MO.plus v r
-      eval0 :: r -> Delta r -> ST s ()
+      eval0 :: r -> DeltaScalar r -> ST s ()
       eval0 !r = \case
-        Zero -> return ()
-        Scale k d -> eval0 (k * r) d
-        Add d e -> eval0 r d >> eval0 r e
-        Var (DeltaId i) -> VM.modify store0 (+ r) i
+        ZeroScalar -> return ()
+        ScaleScalar k d -> eval0 (k * r) d
+        AddScalar d e -> eval0 r d >> eval0 r e
+        VarScalar (DeltaId i) -> VM.modify store0 (+ r) i
+
         Dot1 vr vd -> eval1 (HM.scale r vr) vd
         SumElements1 vd n -> eval1 (HM.konst r n) vd
         Index1 d i k -> eval1 (HM.konst 0 k V.// [(i, r)]) d
-
-        -- Most of the impossible cases will be ruled out by GADT
-        -- once the conflation fo parameterizations is cleared.
-        Seq1{} -> error "buildVectors: Seq1 can't result in a scalar"
-        Konst1{} -> error "buildVectors: Konst1 can't result in a scalar"
-        Append1{} -> error "buildVectors: Append1 can't result in a scalar"
-        Slice1{} -> error "buildVectors: Slice1 can't result in a scalar"
-        Seq2{} -> error "buildVectors: Seq2 can't result in a scalar"
-        Transpose2{} ->
-          error "buildVectors: Transpose2 can't result in a scalar"
-        M_VD2{} -> error "buildVectors: MxVD2 can't result in a scalar"
-        MD_V2{} -> error "buildVectors: MDxV2 can't result in a scalar"
-        M_MD2{} -> error "buildVectors: MxMD2 can't result in a scalar"
-        MD_M2{} -> error "buildVectors: MDxM2 can't result in a scalar"
-        SumRows2{} -> error "buildVectors: SumRows2 can't result in a scalar"
-        SumColumns2{} ->
-          error "buildVectors: SumColumns2 can't result in a scalar"
-        AsRow2{} -> error "buildVectors: AsRow2 can't result in a scalar"
-        AsColumn2{} -> error "buildVectors: AsColumn2 can't result in a scalar"
-        RowAppend2{} ->
-          error "buildVectors: RowAppend2 can't result in a scalar"
-        ColumnAppend2{} ->
-          error "buildVectors: ColumnAppend2 can't result in a scalar"
-        RowSlice2{} -> error "buildVectors: RowSlice2 can't result in a scalar"
-        ColumnSlice2{} ->
-          error "buildVectors: ColumnSlice2 can't result in a scalar"
-      eval1 :: Vector r -> Delta (Vector r) -> ST s ()
+      eval1 :: Vector r -> DeltaVector r -> ST s ()
       eval1 !r = \case
-        Zero -> return ()
-        Scale k d -> eval1 (k * r) d
-        Add d e -> eval1 r d >> eval1 r e
-        Var (DeltaId i) -> VM.modify store1 (addToVector r) i
+        ZeroVector -> return ()
+        ScaleVector k d -> eval1 (k * r) d
+        AddVector d e -> eval1 r d >> eval1 r e
+        VarVector (DeltaId i) -> VM.modify store1 (addToVector r) i
+
         Seq1 vd -> V.imapM_ (\i d -> eval0 (r V.! i) d) vd
         Konst1 d -> V.mapM_ (`eval0` d) r
         Append1 d k e -> eval1 (V.take k r) d >> eval1 (V.drop k r) e
@@ -201,18 +208,13 @@ buildVectors st dTopLevel = do
         MD_V2 md row -> eval2 (MO.MatrixOuter Nothing (Just r) (Just row)) md
         SumRows2 dm ncols -> eval2 (MO.asColumn r ncols) dm
         SumColumns2 dm nrows -> eval2 (MO.asRow r nrows) dm
-
-        Dot1{} -> error "buildVectors: unboxed vectors of vectors not possible"
-        SumElements1{} ->
-          error "buildVectors: unboxed vectors of vectors not possible"
-        Index1{} ->
-          error "buildVectors: unboxed vectors of vectors not possible"
-      eval2 :: MO.MatrixOuter r -> Delta (Matrix r) -> ST s ()
+      eval2 :: MO.MatrixOuter r -> DeltaMatrix r -> ST s ()
       eval2 !r = \case
-        Zero -> return ()
-        Scale k d -> eval2 (MO.multiplyWithOuter k r) d
-        Add d e -> eval2 r d >> eval2 r e
-        Var (DeltaId i) -> VM.modify store2 (addToMatrix r) i
+        ZeroMatrix -> return ()
+        ScaleMatrix k d -> eval2 (MO.multiplyWithOuter k r) d
+        AddMatrix d e -> eval2 r d >> eval2 r e
+        VarMatrix (DeltaId i) -> VM.modify store2 (addToMatrix r) i
+
         Seq2 md -> zipWithM_ eval1 (MO.toRows r) (V.toList md)
         Transpose2 md -> eval2 (MO.transpose r) md  -- TODO: test!
         M_MD2 m md -> zipWithM_ (\rRow row -> eval1 rRow (MD_V2 md row))
@@ -243,12 +245,6 @@ buildVectors st dTopLevel = do
                 `MO.columnAppend`
                 MO.konst 0 nrows (ncols - i - n))
                 d
-
-        Dot1{} -> error "buildVectors: unboxed vectors of vectors not possible"
-        SumElements1{} ->
-          error "buildVectors: unboxed vectors of vectors not possible"
-        Index1{} ->
-          error "buildVectors: unboxed vectors of vectors not possible"
   eval0 1 dTopLevel  -- dt is 1 or hardwired in f
   let evalUnlessZero :: DeltaBinding r -> ST s ()
       evalUnlessZero (DScalar (DeltaId i) d) = do
