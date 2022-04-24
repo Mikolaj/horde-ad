e3e1047c62fd7288e38c1a88f9b213136beb6653
Tom Ellis <tom-git@jaguarpaw.co.uk>
2022-04-24 13:01:39 +0100

    Works nicely

--- src/HordeAd/Core/Again.hs
+++ src/HordeAd/Core/Again.hs
@@ -1,143 +1,171 @@
-{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
 
-module HordeAd.Core.Again (Dim(..)) where
+module HordeAd.Core.Again (module HordeAd.Core.Again) where
 
-import Prelude
-import Data.Kind (Type)
 import Control.Monad.Trans.State
 import Data.Functor.Identity
-import Numeric.LinearAlgebra (add, scale, dot, konst, atIndex,
-                              Numeric, Vector)
+import Data.Kind (Type)
+import Numeric.LinearAlgebra
+  ( Numeric,
+    Vector,
+    add,
+    atIndex,
+    dot,
+    konst,
+    scale,
+  )
+import Prelude
 
-class KnownDim (d :: Dim) where
-  known :: SDim d
+class Known t where
+  known :: t
 
-data Dim = D0 | D1
+knownP :: Known t => proxy t -> t
+knownP _ = known
 
-data SDim (a :: Dim) where
-  SD0 :: SDim 'D0
-  SD1 :: SDim 'D1
+data IsScalarOf (scalar :: Type) (t :: Type) where
+  SScalar :: IsScalarOf scalar scalar
+  SVector :: IsScalarOf scalar (Vector scalar)
 
-data DeltaF (r :: Type) (delta :: Dim -> Type) (d :: Dim) where
-  Zero0 :: DeltaF r delta 'D0
+data DeltaF (s :: Type) (dual :: Type -> Type) (t :: Type) where
+  Zero0 :: DeltaF s dual s
+  Add0 :: dual s -> dual s -> DeltaF s dual s
+  Scale0 :: s -> dual s -> DeltaF s dual s
+  Index0 :: dual (Vector s) -> Int -> DeltaF s dual s
+  Add1 :: dual (Vector s) -> dual (Vector s) -> DeltaF s dual (Vector s)
+  Scale1 :: s -> dual (Vector s) -> DeltaF s dual (Vector s)
+  Konst1 :: dual s -> Int -> DeltaF s dual (Vector s)
+  Dot1 :: dual (Vector s) -> dual (Vector s) -> DeltaF s dual s
 
-  Add :: delta d -> delta d -> DeltaF r delta d
-  Scale :: r -> delta d -> DeltaF r delta d
-
-  Index0 :: delta 'D1 -> Int -> DeltaF r delta 'D0
-  Konst1 :: delta 'D0 -> Int -> DeltaF r delta 'D1
-  Dot1 :: delta d -> delta d -> DeltaF r delta 'D0
-
-newtype DeltaId (d :: Dim) where
-  DeltaId :: Int -> DeltaId d
+newtype DeltaId (r :: Type) where
+  DeltaId :: Int -> DeltaId r
 
 succDeltaId :: DeltaId d -> DeltaId d
 succDeltaId (DeltaId i) = DeltaId (i + 1)
 
 data DeltaBinding r where
-  DeltaBinding :: SDim d -> DeltaId d -> Delta r d -> DeltaBinding r
+  DeltaBinding :: s `IsScalarOf` d -> DeltaId d -> Delta r d -> DeltaBinding r
 
-data Delta (r :: Type) (d :: Dim) where
-  Delta :: DeltaF r (Delta r) d -> Delta r d
-  Var :: DeltaId d -> Delta r d
+data Delta (s :: Type) (dual :: Type) where
+  Delta :: DeltaF s (Delta s) dual -> Delta s dual
+  Var :: DeltaId dual -> Delta s dual
 
-data DeltaState r = DeltaState
-  { deltaCounter0 :: DeltaId 'D0
-  , deltaCounter1 :: DeltaId 'D1
-  , deltaBindings :: [DeltaBinding r]
+data DeltaState s = DeltaState
+  { deltaCounter0 :: DeltaId s,
+    deltaCounter1 :: DeltaId (Vector s),
+    deltaBindings :: [DeltaBinding s]
   }
 
-newtype DualMonadGradient r a = DualMonadGradient
-  { runDualMonadGradient :: State (DeltaState r) a }
+newtype DualMonadGradient s t = DualMonadGradient
+  {runDualMonadGradient :: State (DeltaState s) t}
   deriving newtype (Monad, Functor, Applicative)
 
-class Monad m => DeltaMonad m (delta :: Dim -> Type) where
-  deltaLet :: SDim d -> delta d -> m (delta d)
+class Monad m => DeltaMonad s (dual :: Type -> Type) m | m -> s where
+  deltaLet :: s `IsScalarOf` t -> dual t -> m (dual t)
 
-instance DeltaMonad (DualMonadGradient r) (Delta r) where
+instance DeltaMonad s (Delta s) (DualMonadGradient s) where
   deltaLet sd delta = DualMonadGradient $ do
     st <- get
     case sd of
-          SD0 -> do
-            put (st { deltaCounter0 = succDeltaId (deltaCounter0 st)
-                    , deltaBindings = DeltaBinding sd (deltaCounter0 st) delta : deltaBindings st
-                    })
-            pure (Var (deltaCounter0 st))
-          SD1 -> do
-            put (st { deltaCounter1 = succDeltaId (deltaCounter1 st)
-                    , deltaBindings = DeltaBinding sd (deltaCounter1 st) delta : deltaBindings st
-                    })
-            pure (Var (deltaCounter1 st))
+      SScalar -> do
+          put
+            ( st
+                { deltaCounter0 = succDeltaId (deltaCounter0 st),
+                  deltaBindings = DeltaBinding sd (deltaCounter0 st) delta : deltaBindings st
+                }
+            )
+          pure (Var (deltaCounter0 st))
+      SVector -> do
+          put
+            ( st
+                { deltaCounter1 = succDeltaId (deltaCounter1 st),
+                  deltaBindings = DeltaBinding SVector (deltaCounter1 st) delta : deltaBindings st
+                }
+            )
+          pure (Var (deltaCounter1 st))
 
-newtype DualMonadValue a = DualMonadValue
-  { runDualMonadValue :: Identity a }
+newtype DualMonadValue r a = DualMonadValue
+  {runDualMonadValue :: Identity a}
   deriving newtype (Monad, Functor, Applicative)
 
-data Unit (d :: Dim) = Unit
+data Unit (t :: Type) = Unit
 
-instance DeltaMonad DualMonadValue Unit where
+instance DeltaMonad r Unit (DualMonadValue r) where
   deltaLet _ = pure
 
-dLet :: forall (t :: Dim -> Type) a d m.
-        (KnownDim d, DeltaMonad m t)
-     => Dual a (t d)
-     -> m (Dual a (t d))
-dLet  (Dual x y) = do
-  y' <- deltaLet known y
+dLetS ::
+  forall (d :: Type -> Type) t m s.
+  DeltaMonad s d m =>
+  s `IsScalarOf` t ->
+  Dual t (d t) ->
+  m (Dual t (d t))
+dLetS si (Dual x y) = do
+  y' <- deltaLet si y
   pure (Dual x y')
+
+dLet ::
+  forall s d t m.
+  (DeltaMonad s d m, Known (s `IsScalarOf` t)) =>
+  Dual t (d t) ->
+  m (Dual t (d t))
+dLet = dLetS known
 
 data Dual a b = Dual a b
 
-class Ops r t | t -> r where
-  ops :: DeltaF r t d -> t d
+class Ops r d | d -> r where
+  ops :: DeltaF r d t -> d t
 
-zero :: (Num r, Ops r t) => Dual r (t 'D0)
-zero = Dual 0 (ops Zero0)
-
-(.+) :: (Ops r t, Num a, KnownDim d, DeltaMonad m t) =>
-        Dual a (t d)
-     -> Dual a (t d)
-     -> m (Dual a (t d))
-Dual x x' .+ Dual y y' = dLet $
-  Dual (x + y) (ops (Add x' y'))
-
-(.*) :: (Ops a t, Num a, KnownDim d, DeltaMonad m t) =>
-        Dual a (t d) -> Dual a (t d) -> m (Dual a (t d))
-Dual x x' .* Dual y y' = dLet $
-  Dual (x * y) (ops (Add (ops (Scale y x')) (ops (Scale x y'))))
-
-instance Ops r (Delta r) where
-  ops = Delta
-
-data Concrete r (d :: Dim) where
-  C0 :: r -> Concrete r 'D0
-  C1 :: Vector r -> Concrete r 'D1
+data Concrete r (t :: Type) where
+  C0 :: r -> Concrete r r
+  C1 :: Vector r -> Concrete r (Vector r)
 
 instance (Num r, Numeric r) => Ops r (Concrete r) where
   ops = \case
     Zero0 -> C0 0
-    Add (C0 x1) (C0 x2) -> C0 (x1 + x2)
-    Add (C1 x1) (C1 x2) -> C1 (x1 `add` x2)
-    Scale r (C0 x) -> C0 (r * x)
-    Scale r (C1 x) -> C1 (scale r x)
-
+    Add0 (C0 x1) (C0 x2) -> C0 (x1 + x2)
+    Scale0 r (C0 x) -> C0 (r * x)
     Index0 (C1 v) i -> C0 (atIndex v i)
+    Add1 (C1 x1) (C1 x2) -> C1 (x1 `add` x2)
+    Scale1 r (C1 x) -> C1 (scale r x)
     Konst1 (C0 k) i -> C1 (konst k i)
     Dot1 (C1 v1) (C1 v2) -> C0 (v1 `dot` v2)
-    Dot1 (C0 v1) (C0 v2) -> C0 (v1 * v2)
 
+zero :: (Num r, Ops r d) => Dual r (d r)
+zero = Dual 0 (ops Zero0)
 
-foo :: (Ops r t, DeltaMonad m t, KnownDim d, Num r)
-    => Dual r (t d) -> Dual r (t d) -> m (Dual r (t d))
+instance Ops r (Delta r) where
+  ops = Delta
+
+foo ::
+  (DeltaMonad s d m, Num t, Ops t d, Known (s `IsScalarOf` t)) =>
+  Dual t (d t) ->
+  Dual t (d t) ->
+  m (Dual t (d t))
 foo x y = do
   x2 <- x .* x
   x2y <- x2 .* y
-  x2y_p_y <- x2y .+ y
-  pure x2y_p_y
+  x2y .+ y
+
+(.+) ::
+  (DeltaMonad s d m, Known (s `IsScalarOf` t), Num t, Ops t d) =>
+  Dual t (d t) ->
+  Dual t (d t) ->
+  m (Dual t (d t))
+Dual x x' .+ Dual y y' =
+  dLet $
+    Dual (x + y) (ops (Add0 x' y'))
+
+(.*) ::
+  (DeltaMonad s dual m, Known (s `IsScalarOf` t), Num t, Ops t dual) =>
+  Dual t (dual t) ->
+  Dual t (dual t) ->
+  m (Dual t (dual t))
+Dual x x' .* Dual y y' =
+  dLet $
+    Dual (x * y) (ops (Add0 (ops (Scale0 y x')) (ops (Scale0 x y'))))
