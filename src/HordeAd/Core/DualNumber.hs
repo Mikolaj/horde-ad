5f96956cb6a2301c15ade186171699cb4993e19d
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2022-05-16 21:15:08 +0200

    simplify logBase

--- src/HordeAd/Core/DualNumber.hs
+++ src/HordeAd/Core/DualNumber.hs
@@ -102,7 +102,7 @@ instance (Floating a, IsPrimal d a) => Floating (DualNumber d a) where
   sqrt = undefined  -- TODO
   D u u' ** D v v' = D (u ** v) (dAdd (dScale (v * (u ** (v - 1))) u')
                                       (dScale ((u ** v) * log u) v'))
-  logBase (D u u') (D v v') = (log (D v v')) / (log (D u u'))
+  logBase x y = (log y) / (log x)
   sin (D u u') = D (sin u) (dScale (cos u) u')
   cos (D u u') = D (cos u) (dScale (- (sin u)) u')
   tan (D u u') = let cosU = cos u
