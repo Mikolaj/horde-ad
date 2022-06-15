fb1418d7d3cfa4761a69d53bce6210de63cb09d8
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2022-06-15 14:40:13 +0200

    implement eq-epsilon option

--- test/ShortTestForCI.hs
+++ test/ShortTestForCI.hs
@@ -7,9 +7,11 @@
 module Main (main) where
 
 import Prelude
+import Data.Typeable
 
 import qualified System.IO as SIO
 import           Test.Tasty
+import           Test.Tasty.Options
 
 #if defined(VERSION_ghc_typelits_natnormalise)
 import qualified TestMnistCNN
@@ -19,12 +21,23 @@ import qualified TestSimpleDescent
 import qualified TestSingleGradient
 #endif
 
+newtype EqEpsilon = EqEpsilon Double
+  deriving Typeable
+
+instance IsOption EqEpsilon where
+  defaultValue = EqEpsilon 1e-4
+  parseValue = fmap EqEpsilon . safeRead
+  optionName = return "eq-epsilon"
+  optionHelp = return "Epsilon to use for floating point comparisons: abs(a-b) < epsilon"
+
 main :: IO ()
 main = do
   -- Limit interleaving of characters in parallel tests.
   SIO.hSetBuffering SIO.stdout SIO.LineBuffering
   SIO.hSetBuffering SIO.stderr SIO.LineBuffering
-  defaultMain tests
+  defaultMainWithIngredients (ingredients : defaultIngredients) tests
+  where
+    ingredients = includingOptions [Option (Proxy :: Proxy EqEpsilon)]
 
 tests :: TestTree
 tests = testGroup "Short tests for CI" $
