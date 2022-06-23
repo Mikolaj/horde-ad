71540f82c656d8a10344c1e37b8a5b51593db2ba
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2022-06-23 09:20:05 +0200

    implement support for eq-epsilon

--- test/ExtremelyLongTest.hs
+++ test/ExtremelyLongTest.hs
@@ -7,10 +7,14 @@
 module Main (main) where
 
 import Prelude
+import Data.Proxy
 
 import qualified System.IO as SIO
 import           Test.Tasty
+import           Test.Tasty.Options
+import           Test.Tasty.Runners
 
+import           TestCommonEqEpsilon
 import qualified TestConditionalSynth
 import qualified TestOutdated
 
@@ -27,7 +31,11 @@ main = do
   -- Limit interleaving of characters in parallel tests.
   SIO.hSetBuffering SIO.stdout SIO.LineBuffering
   SIO.hSetBuffering SIO.stderr SIO.LineBuffering
-  defaultMain tests
+  opts <- parseOptions (ingredients : defaultIngredients) tests
+  setEpsilonEq (lookupOption opts :: EqEpsilon)
+  defaultMainWithIngredients (ingredients : defaultIngredients) tests
+  where
+    ingredients = includingOptions [Option (Proxy :: Proxy EqEpsilon)]
 
 tests :: TestTree
 tests = testGroup "Tests" $
