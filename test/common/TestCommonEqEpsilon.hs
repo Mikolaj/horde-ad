b18d7169cce5db3c809586dcf81197b1c9ef50fc
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2022-06-24 15:48:03 +0200

    implement (@?~) infix operator; generalize

--- test/common/TestCommonEqEpsilon.hs
+++ test/common/TestCommonEqEpsilon.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-module TestCommonEqEpsilon (EqEpsilon, setEpsilonEq, assertClose, assertCloseElem, assertCloseList) where
+module TestCommonEqEpsilon (EqEpsilon, setEpsilonEq, assertClose, assertCloseElem, assertCloseList, (@?~)) where
 
 import Prelude
 import Data.Typeable
@@ -10,7 +10,7 @@ import System.IO.Unsafe
 import Test.Tasty.HUnit
 import Test.Tasty.Options
 
-newtype EqEpsilon = EqEpsilon Double
+newtype EqEpsilon = EqEpsilon Rational
   deriving (Typeable, Num, Fractional)
 
 instance IsOption EqEpsilon where
@@ -20,11 +20,11 @@ instance IsOption EqEpsilon where
   optionHelp = return $ "Epsilon to use for floating point comparisons: abs(a-b) < epsilon . Default: " ++ show eqEpsilonDefault
 
 -- Default value for eqEpsilonRef
-eqEpsilonDefault :: Double
+eqEpsilonDefault :: Rational
 eqEpsilonDefault = 1e-6
 
 -- Ugly global epsilon used to compare floating point values.
-eqEpsilonRef :: IORef Double
+eqEpsilonRef :: IORef Rational
 {-# NOINLINE eqEpsilonRef #-}
 eqEpsilonRef = unsafePerformIO $ newIORef eqEpsilonDefault
 
@@ -38,20 +38,22 @@ setEpsilonEq (EqEpsilon x) = atomicWriteIORef eqEpsilonRef x
 --
 -- If the prefix is the empty string (i.e., @\"\"@), then the prefix is omitted
 -- and only the expected and actual values are output.
-assertClose :: String -- ^ The message prefix
-            -> Double -- ^ The expected value
-            -> Double -- ^ The actual value
+assertClose :: forall a. (Fractional a, Ord a, Show a, HasCallStack)
+            => String -- ^ The message prefix
+            -> a      -- ^ The expected value
+            -> a      -- ^ The actual value
             -> Assertion
 assertClose preface expected actual = do
   eqEpsilon <- readIORef eqEpsilonRef
-  assertBool msg (abs(expected-actual) < eqEpsilon)
+  assertBool msg (abs(expected-actual) < (fromRational eqEpsilon))
   where msg = (if null preface then "" else preface ++ "\n") ++
                "expected: " ++ show expected ++ "\n but got: " ++ show actual
 
 -- | Asserts that the specified actual floating point value is close to at least one of the expected values.
-assertCloseElem :: String   -- ^ The message prefix
-                -> [Double] -- ^ The expected values
-                -> Double   -- ^ The actual value
+assertCloseElem :: forall a. (Fractional a, Ord a, Show a, HasCallStack)
+                => String   -- ^ The message prefix
+                -> [a]      -- ^ The expected values
+                -> a        -- ^ The actual value
                 -> Assertion
 assertCloseElem preface expected actual = do
   eqEpsilon <- readIORef eqEpsilonRef
@@ -59,15 +61,16 @@ assertCloseElem preface expected actual = do
   where
     msg = (if null preface then "" else preface ++ "\n") ++
            "wrong result: " ++ show actual ++ " is expected to be a member of " ++ show expected
-    go_assert :: Double -> [Double] -> Assertion
+    go_assert :: Rational -> [a] -> Assertion
     go_assert _ [] = assertFailure msg
-    go_assert eqEps (h1:t1) =
-      if abs(h1-actual) < eqEps then assertClose msg h1 actual else go_assert eqEps t1
+    go_assert eqEps (h:t) =
+      if abs(h-actual) < (fromRational eqEps) then assertClose msg h actual else go_assert eqEps t
 
 -- | Asserts that the specified actual floating point value list is close to the expected value.
-assertCloseList :: String   -- ^ The message prefix
-                -> [Double] -- ^ The expected value
-                -> [Double] -- ^ The actual value
+assertCloseList :: forall a. (Fractional a, Ord a, Show a, HasCallStack)
+                => String   -- ^ The message prefix
+                -> [a]      -- ^ The expected value
+                -> [a]      -- ^ The actual value
                 -> Assertion
 assertCloseList preface expected actual =
   go_assert expected actual
@@ -76,9 +79,20 @@ assertCloseList preface expected actual =
     len2 :: Int = length actual
     msgneq :: String = (if null preface then "" else preface ++ "\n") ++
                         "expected " ++ show len1 ++ " elements, but got " ++ show len2
-    go_assert :: [Double] -> [Double] -> Assertion
+    go_assert :: [a] -> [a] -> Assertion
     go_assert [] [] = assertBool preface True
     go_assert [] (_:_) = assertFailure msgneq
     go_assert (_:_) [] = assertFailure msgneq
     go_assert (h1:t1) (h2:t2) =
       assertClose preface h1 h2 >> go_assert t1 t2
+
+infix  1 @?~
+
+-- | Asserts that the specified actual value is close to the expected value
+--   (with the actual value on the left-hand side).
+(@?~)
+  :: (Fractional a, Ord a, Show a, HasCallStack)
+  => a -- ^ The actual value
+  -> a -- ^ The expected value
+  -> Assertion
+actual @?~ expected = assertClose "" expected actual
