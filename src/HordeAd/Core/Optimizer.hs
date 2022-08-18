8d64c50199513bff77640e850f1ab1776552dc88
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2022-08-18 17:16:52 +0200

    Use the counters' final state
    All initial tests pass, but sharing is not preserved,
    so the blowup test blows up.

--- src/HordeAd/Core/Optimizer.hs
+++ src/HordeAd/Core/Optimizer.hs
@@ -9,13 +9,15 @@ module HordeAd.Core.Optimizer
 
 import Prelude
 
-import Numeric.LinearAlgebra (Vector)
+import System.IO.Unsafe (unsafePerformIO)
 
 import HordeAd.Core.DualNumber
 import HordeAd.Core.Engine
 import HordeAd.Core.OptimizerTools
 import HordeAd.Core.PairOfVectors (DualNumberVariables, makeDualNumberVariables)
 
+-- TODO: change the type to IO, but this requires a rewrite of all
+-- test glue code; also remove NOINLINE
 -- | Simple Gradient Descent.
 gdSimple :: forall r. HasDelta r
          => r
@@ -23,6 +25,7 @@ gdSimple :: forall r. HasDelta r
          -> Int  -- ^ requested number of iterations
          -> Domains r  -- ^ initial parameters
          -> Domains r
+{-# NOINLINE gdSimple #-}
 gdSimple gamma f n0 parameters0 = go n0 parameters0 where
   -- Pre-allocating the vars once, vs gradually allocating on the spot in each
   -- gradient computation, initially incurs overhead (looking up in a vector),
@@ -33,10 +36,12 @@ gdSimple gamma f n0 parameters0 = go n0 parameters0 where
   go 0 parameters = parameters
   go n parameters =
     let variables = makeDualNumberVariables parameters varDeltas
-        gradients = fst $ dReverseGeneral 1 variables f
+        gradients = unsafePerformIO $ fst <$> dReverseGeneral 1 variables f
         parametersNew = updateWithGradient gamma parameters gradients
     in go (pred n) parametersNew
 
+-- TODO: change the type to IO, but this requires a rewrite of all
+-- test glue code; also remove NOINLINE
 -- | Stochastic Gradient Descent.
 sgd :: forall r a. HasDelta r
     => r
@@ -44,23 +49,27 @@ sgd :: forall r a. HasDelta r
     -> [a]  -- ^ training data
     -> Domains r  -- ^ initial parameters
     -> (Domains r, r)
+{-# NOINLINE sgd #-}
 sgd gamma f trainingData parameters0 = go trainingData parameters0 where
   varDeltas = generateDeltaVars parameters0
   go :: [a] -> Domains r -> (Domains r, r)
   go [] parameters = (parameters, 0)
   go (a : rest) parameters =
     let variables = makeDualNumberVariables parameters varDeltas
-        (gradients, valueNew) = dReverseGeneral 1 variables (f a)
+        (gradients, valueNew) =
+          unsafePerformIO $ dReverseGeneral 1 variables (f a)
         !parametersNew = updateWithGradient gamma parameters gradients
     in if null rest
        then (parametersNew, valueNew)
        else go rest parametersNew
+{-
 {-# SPECIALIZE sgd
   :: Double
   -> ((Vector Double, Vector Double) -> DualNumberVariables 'DModeGradient Double -> DualMonadGradient Double (DualNumber 'DModeGradient Double))
   -> [(Vector Double, Vector Double)]
   -> Domains Double
   -> (Domains Double, Double) #-}
+-}
 
 sgdAdam :: forall r a. HasDelta r
         => (a -> DualNumberVariables 'DModeGradient r -> DualMonadGradient r (DualNumber 'DModeGradient r))
@@ -70,6 +79,8 @@ sgdAdam :: forall r a. HasDelta r
         -> (Domains r, StateAdam r)
 sgdAdam = sgdAdamArgs defaultArgsAdam
 
+-- TODO: change the type to IO, but this requires a rewrite of all
+-- test glue code; also remove NOINLINE
 sgdAdamArgs :: forall r a. HasDelta r
             => ArgsAdam r
             -> (a -> DualNumberVariables 'DModeGradient r
@@ -78,6 +89,7 @@ sgdAdamArgs :: forall r a. HasDelta r
             -> Domains r
             -> StateAdam r
             -> (Domains r, StateAdam r)
+{-# NOINLINE sgdAdamArgs #-}
 sgdAdamArgs argsAdam f trainingData parameters0 stateAdam0 =
   go trainingData parameters0 stateAdam0
  where
@@ -86,7 +98,7 @@ sgdAdamArgs argsAdam f trainingData parameters0 stateAdam0 =
   go [] parameters stateAdam = (parameters, stateAdam)
   go (a : rest) parameters stateAdam =
     let variables = makeDualNumberVariables parameters varDeltas
-        gradients = fst $ dReverseGeneral 1 variables (f a)
+        gradients = unsafePerformIO $ fst <$> dReverseGeneral 1 variables (f a)
         (parametersNew, stateAdamNew) =
           updateWithGradientAdam argsAdam stateAdam parameters gradients
     in go rest parametersNew stateAdamNew
