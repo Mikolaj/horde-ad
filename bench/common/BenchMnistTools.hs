98706f5559cbd56a37f9ca03ed4d0dcad551eb90
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2022-08-24 10:14:52 +0200

    Setting -fno-cse is apparently not needed any more

--- bench/common/BenchMnistTools.hs
+++ bench/common/BenchMnistTools.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DataKinds, TypeFamilies #-}
 {-# OPTIONS_GHC -Wno-missing-export-lists #-}
 -- Needed due to unsafePerformIO:
-{-# OPTIONS_GHC -fno-full-laziness -fno-cse #-}
+{-# OPTIONS_GHC -fno-full-laziness #-}
 module BenchMnistTools where
 
 import Prelude
