c841d0f5f2f4b6281c7dbc3e1c0a1f40144b8836
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2022-08-29 17:48:13 +0200

    Turn DualNumberVariables from type to data

--- src/HordeAd/Core/PairOfVectors.hs
+++ src/HordeAd/Core/PairOfVectors.hs
@@ -9,7 +9,7 @@
 -- of parameters and, in case of dual components that are delta-expressions,
 -- with @Delta@ variables assigned to each.
 module HordeAd.Core.PairOfVectors
-  ( DualNumberVariables
+  ( DualNumberVariables(..)
   , makeDualNumberVariables, var0, vars, var1, var2, varX, varS
   , ifoldlDual', foldlDual'
   ) where
@@ -30,16 +30,16 @@ import HordeAd.Core.DualNumber
 -- in an efficient way (especially, or only, with gradient descent,
 -- where the vectors are reused in some ways).
 
-type DualNumberVariables d r =
-  ( Domain0 r
-  , Data.Vector.Vector (Dual d r)
-  , Domain1 r
-  , Data.Vector.Vector (Dual d (Vector r))
-  , Domain2 r
-  , Data.Vector.Vector (Dual d (Matrix r))
-  , DomainX r
-  , Data.Vector.Vector (Dual d (OT.Array r))
-  )
+data DualNumberVariables d r = DualNumberVariables
+  { inputPrimal0 :: Domain0 r
+  , inputDual0   :: Data.Vector.Vector (Dual d r)
+  , inputPrimal1 :: Domain1 r
+  , inputDual1   :: Data.Vector.Vector (Dual d (Vector r))
+  , inputPrimal2 :: Domain2 r
+  , inputDual2   :: Data.Vector.Vector (Dual d (Matrix r))
+  , inputPrimalX :: DomainX r
+  , inputDualX   :: Data.Vector.Vector (Dual d (OT.Array r))
+  }
 
 makeDualNumberVariables
   :: Domains r
@@ -51,29 +51,34 @@ makeDualNumberVariables
 {-# INLINE makeDualNumberVariables #-}
 makeDualNumberVariables (params0, params1, params2, paramsX)
                         (vs0, vs1, vs2, vsX)
-  = (params0, vs0, params1, vs1, params2, vs2, paramsX, vsX)
+  = DualNumberVariables params0 vs0 params1 vs1 params2 vs2 paramsX vsX
 
 var0 :: IsScalar d r => DualNumberVariables d r -> Int -> DualNumber d r
-var0 (vValue, vVar, _, _, _, _, _, _) i = D (vValue V.! i) (vVar V.! i)
+{-# INLINE var0 #-}
+var0 DualNumberVariables{..} i = D (inputPrimal0 V.! i) (inputDual0 V.! i)
 
 -- Unsafe, but handy for toy examples.
 vars :: IsScalar d r => DualNumberVariables d r -> [DualNumber d r]
-vars vec = map ( var0 vec) [0 ..]
+vars vec = map (var0 vec) [0 ..]
 
 var1 :: DualNumberVariables d r -> Int -> DualNumber d (Vector r)
-var1 (_, _, vValue, vVar, _, _, _, _) i = D (vValue V.! i) (vVar V.! i)
+{-# INLINE var1 #-}
+var1 DualNumberVariables{..} i = D (inputPrimal1 V.! i) (inputDual1 V.! i)
 
 var2 :: DualNumberVariables d r -> Int -> DualNumber d (Matrix r)
-var2 (_, _, _, _, vValue, vVar, _, _) i = D (vValue V.! i) (vVar V.! i)
+{-# INLINE var2 #-}
+var2 DualNumberVariables{..} i = D (inputPrimal2 V.! i) (inputDual2 V.! i)
 
 varX :: DualNumberVariables d r -> Int -> DualNumber d (OT.Array r)
-varX (_, _, _, _, _, _, vValue, vVar) i = D (vValue V.! i) (vVar V.! i)
+{-# INLINE varX #-}
+varX DualNumberVariables{..} i = D (inputPrimalX V.! i) (inputDualX V.! i)
 
 varS :: (IsScalar d r, OS.Shape sh)
      => DualNumberVariables d r -> Int -> DualNumber d (OS.Array sh r)
-varS (_, _, _, _, _, _, vValue, vVar) i =
+{-# INLINE varS #-}
+varS DualNumberVariables{..} i =
 #if defined(VERSION_ghc_typelits_natnormalise)
-  inline fromXS $ D (vValue V.! i) (vVar V.! i)
+  inline fromXS $ D (inputPrimalX V.! i) (inputDualX V.! i)
 #else
   undefined
 #endif
@@ -84,12 +89,12 @@ ifoldlDual' :: forall a d r. IsScalar d r
              -> DualNumberVariables d r
              -> a
 {-# INLINE ifoldlDual' #-}
-ifoldlDual' f a (vecR, vecD, _, _, _, _, _, _) = do
+ifoldlDual' f a DualNumberVariables{..} = do
   let g :: a -> Int -> r -> a
       g !acc i valX =
-        let !b = D valX (vecD V.! i)
+        let !b = D valX (inputDual0 V.! i)
         in f acc i b
-  V.ifoldl' g a vecR
+  V.ifoldl' g a inputPrimal0
 
 foldlDual' :: forall a d r. IsScalar d r
             => (a -> DualNumber d r -> a)
@@ -97,9 +102,9 @@ foldlDual' :: forall a d r. IsScalar d r
             -> DualNumberVariables d r
             -> a
 {-# INLINE foldlDual' #-}
-foldlDual' f a (vecR, vecD, _, _, _, _, _, _) = do
+foldlDual' f a DualNumberVariables{..} = do
   let g :: a -> Int -> r -> a
       g !acc i valX =
-        let !b = D valX (vecD V.! i)
+        let !b = D valX (inputDual0 V.! i)
         in f acc b
-  V.ifoldl' g a vecR
+  V.ifoldl' g a inputPrimal0
