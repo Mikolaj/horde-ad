ba8a981bc3b5b1cfb54b3541c8f75dc3af8e363c
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2022-09-01 15:52:58 +0200

    Add MNIST benchmark with realistic dataset sizes

---
+++ bench/RealisticMnistBench.hs
@@ -0,0 +1,20 @@
+module Main (main) where
+
+import Prelude
+
+import Criterion.Main
+import System.Random
+
+import HordeAd.Tool.MnistData
+
+import BenchMnistTools
+
+main :: IO ()
+main = do
+  testData0 <- loadMnistData testGlyphsPath testLabelsPath  -- 10k total
+  let testData = shuffle (mkStdGen 42) testData0
+  defaultMain
+    [ mnistTrainBGroup2 testData 4000
+    , mnistTrainBGroup2V testData 4000
+    , mnistTrainBGroup2L testData 4000
+    ]
