ae57678f843a36175aa6e64f1ffc2886f3a3bde2
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2022-09-29 13:17:01 +0200

    Comment how the fundeps in AdaptableDomains fail

--- src/HordeAd/External/Adaptor.hs
+++ src/HordeAd/External/Adaptor.hs
@@ -49,7 +49,7 @@ fwd f x ds =
 type Adaptable d r advals vals =
   (AdaptableDomains r vals, AdaptableInputs d r advals)
 
--- TODO: here, @| vals -> r@ fails if the 4-tuple below is 3-tuple instead.
+-- TODO: here, @| vals -> r@ fails when uncommenting the instance below.
 -- Probably associated type families are unavoidable
 -- and then probably we'd have a single class again, not two joined
 -- in the single constraint @Adaptable@.
@@ -90,6 +90,53 @@ instance ADModeAndNum d r
     r1 : r2 : r3 : _ -> [r1, r2, r3]
     _ -> error "fromADInputs in Adaptable r [r]"
 
+{-
+-- The error is
+--
+-- src/HordeAd/External/Adaptor.hs:63:10: error:
+--     Functional dependencies conflict between instance declarations:
+--       instance Numeric r => AdaptableDomains r (r, r, r)
+--         -- Defined at src/HordeAd/External/Adaptor.hs:63:10
+--       instance (Numeric r, OS.Shape sh1, OS.Shape sh2, OS.Shape sh3) =>
+--                AdaptableDomains r (OS.Array sh1 r, OS.Array sh2 r, OS.Array sh3 r)
+--         -- Defined at src/HordeAd/External/Adaptor.hs:93:10
+--
+-- and probably means that fundep is wrong, because from a @OS.Array sh Double@
+-- triple I can deduce either that r is Double by this instance or that
+-- r is @OS.Array sh Double@ by an instance above.
+-- Later on the ambiguity could be resolved, but fundeps don't have a way
+-- to delay resolving such ambiguities.
+-- I hope associated type families, perhaps with the AllowAmbiguousTypes
+-- extension, permit encoding and storing such ambiguities until they
+-- can be resolved. I may be totally wrong in any of this.
+instance ( Numeric r
+         , OS.Shape sh1, OS.Shape sh2, OS.Shape sh3 )
+         => AdaptableDomains r ( OS.Array sh1 r
+                               , OS.Array sh2 r
+                               , OS.Array sh3 r ) where
+  toDomains (a, b, c) =
+    ( V.empty, V.empty, V.empty
+    , V.fromList [ Data.Array.Convert.convert a
+                 , Data.Array.Convert.convert b
+                 , Data.Array.Convert.convert c ] )
+  fromDomains (_, _, _, v) = case V.toList v of
+    a : b : c : _ -> ( toShapedOrDummy a
+                     , toShapedOrDummy b
+                     , toShapedOrDummy c )
+    _ -> error "fromDomains in Adaptable r (S, S, S)"
+
+instance ( ADModeAndNum d r
+         , OS.Shape sh1, OS.Shape sh2, OS.Shape sh3 )
+         => AdaptableInputs d r ( ADVal d (OS.Array sh1 r)
+                                , ADVal d (OS.Array sh2 r)
+                                , ADVal d (OS.Array sh3 r) ) where
+  fromADInputs inputs =
+    let a = atS inputs 0
+        b = atS inputs 1
+        c = atS inputs 2
+    in (a, b, c)
+-}
+
 instance ( Numeric r
          , OS.Shape sh1, OS.Shape sh2, OS.Shape sh3, OS.Shape sh4 )
          => AdaptableDomains r ( OS.Array sh1 r
@@ -107,7 +154,7 @@ instance ( Numeric r
                          , toShapedOrDummy b
                          , toShapedOrDummy c
                          , toShapedOrDummy d )
-    _ -> error "fromDomains in Adaptable r (S, S, S)"
+    _ -> error "fromDomains in Adaptable r (S, S, S, S)"
 
 instance ( ADModeAndNum d r
          , OS.Shape sh1, OS.Shape sh2, OS.Shape sh3, OS.Shape sh4 )
