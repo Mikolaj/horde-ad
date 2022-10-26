7f40b00d22693dc1af41ffb29b604702f1b87533
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2022-10-27 00:47:07 +0200

    Add a missing new simplified/HordeAd.hs file to the repo

---
+++ simplified/HordeAd.hs
@@ -0,0 +1,17 @@
+-- | An API of the library that should be sufficient for general use.
+-- To (ab)use implementation details or to access ready tools for specific
+-- applications, such as fully connect neural networks operating on MNIST
+-- data, some extra imports may be needed not covered here.
+module HordeAd
+  ( module HordeAd.Core.DualNumber
+  , module HordeAd.Core.Engine
+  , module HordeAd.Core.PairOfVectors
+  , module HordeAd.External.Optimizer
+  ) where
+
+import Prelude ()
+
+import HordeAd.Core.DualNumber
+import HordeAd.Core.Engine
+import HordeAd.Core.PairOfVectors
+import HordeAd.External.Optimizer
