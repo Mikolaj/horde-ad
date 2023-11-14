d8dae03c23124386f871cee303e645f43636aec7
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2023-11-14 20:36:23 +0100

    profile

--- Main.hs
+++ Main.hs
@@ -19,7 +19,7 @@ import HordeAd.Core.TensorADVal
 t48 :: Flip OR.Array Double 15
 t48 = Flip $ OR.fromList [3, 1, 2, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2] [18.1,29.1,32.1,40.1,52.0,53.99432,97.1,58.8943200001,18.1,29.1,32.1,40.1,58.0,54.99432,97.1,52.8943200001, 5, 2, 6, 1, -2, 0.92, 0.1, -0.2, 13.1, 9, 8, -4, 34, 2.99432, -33, 26, 2, 2, 2, 2, -0.2,-0.2,-0.2,-0.2,25.0003,-0.2,-0.2,-0.2,25.0003,25.0003,25.0003,25.0003]
 
--- * with profiling on (cabal test simplifiedOnlyTest --enable-optimization -w ~/r/ghc.head/_build/stage1/bin/ghc --constraint="ghc-typelits-knownnat==0.7.10" --constraint="ghc-typelits-natnormalise==0.7.9" --test-options='+RTS -xc' --ghc-options=-fprof-auto-calls --enable-profiling) the test fails both with and without -fspecialise-aggressively, but no stack trace is printed in either case and that's because we get `Segmentation fault (core dumped)`; the same without `--ghc-options=-fprof-auto-calls`
+-- * with profiling on (cabal run simplifiedOnlyTest --enable-optimization -w ~/r/ghc.head/_build/stage1/bin/ghc --constraint="ghc-typelits-knownnat==0.7.10" --constraint="ghc-typelits-natnormalise==0.7.9" --ghc-options=-fprof-auto-calls --enable-profiling) the test fails both with and without -fspecialise-aggressively, but no stack trace is printed in either case (even if the exe is run with `+RTS -xc`) and that's because we get `Segmentation fault (core dumped)` very early; the same without `--ghc-options=-fprof-auto-calls`
 revShort ::
      (AstRanked PrimalSpan Double 15 -> AstRanked PrimalSpan Double 17)
      -> Flip OR.Array Double 15
