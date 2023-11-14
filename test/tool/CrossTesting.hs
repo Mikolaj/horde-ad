c674bd7690f8ed9c440a772864f65b070b34f5a7
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2023-11-14 19:02:58 +0100

    simplify

--- test/tool/CrossTesting.hs
+++ test/tool/CrossTesting.hs
@@ -10,7 +10,6 @@ import qualified Data.Array.DynamicS as OD
 import qualified Data.Array.RankedS as OR
 import           Data.Bifunctor.Flip
 import qualified Data.EnumMap.Strict as EM
-import           Numeric.LinearAlgebra (Numeric)
 
 import HordeAd.Core.Adaptor
 import HordeAd.Core.Ast
@@ -40,5 +39,5 @@ revShort f vals =
       (_, value2) = crevOnDomains dt h parameters
   in value2
 
-t48 :: (Numeric r, Fractional r) => Flip OR.Array r 7
+t48 :: Flip OR.Array Double 7
 t48 = Flip $ OR.fromList [3, 1, 2, 2, 1, 2, 2] [18.1,29.1,32.1,40.1,52.0,53.99432,97.1,58.8943200001,18.1,29.1,32.1,40.1,58.0,54.99432,97.1,52.8943200001, 5, 2, 6, 1, -2, 0.92, 0.1, -0.2, 13.1, 9, 8, -4, 34, 2.99432, -33, 26, 2, 2, 2, 2, -0.2,-0.2,-0.2,-0.2,25.0003,-0.2,-0.2,-0.2,25.0003,25.0003,25.0003,25.0003]
