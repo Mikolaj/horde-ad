58e37ecc93dfa139bbc01ca47512ab3bcb1a5e64
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2023-11-16 14:36:59 +0100

    try

--- src/HordeAd/Internal/OrthotopeOrphanInstances.hs
+++ src/HordeAd/Internal/OrthotopeOrphanInstances.hs
@@ -155,7 +155,12 @@ liftVR2 !op t@(RS.A (RG.A sh oit@(OI.T sst _ vt)))
       then assert (OI.offset oit == OI.offset oiu && V.length vt == V.length vu)
            $ RS.A $ RG.A sh $ oit {OI.values = vt `op` vu}
       else let v = OR.toVector t `op` OR.toVector u
-           in traceShow ("OR.fromVector", valueOf @n, sh, v) $ OR.fromVector @r @n sh v
+           in traceShow ("OR.fromVector", valueOf @n, sh, v)
+              $ OR.fromVector @r @n sh v
+-- this does not repro:
+--            $ RS.A $ RG.fromVector @n sh v
+-- but this is how OR.fromVector is defined (without the type application) at https://hackage.haskell.org/package/orthotope-0.1.2.0/docs/src/Data.Array.Internal.RankedS.html#fromVector
+
 
 liftVR2NoAdapt
   :: (Numeric r, Show r, KnownNat n)
