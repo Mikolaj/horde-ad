f50ab91dbabd3d1b38badfe016881a51c4c11b5d
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2023-11-18 14:24:22 +0100

    77

--- Main.hs
+++ Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedLists #-}
+{-# OPTIONS_GHC -ddump-prep -dsuppress-all #-}
 {-# OPTIONS_GHC -fspecialise-aggressively #-}
 -- | Assorted mostly high rank tensor tests.
 module Main (main) where
@@ -16,24 +17,24 @@ import HordeAd.Core.AstFreshId
 
 import Debug.Trace
 
-t48 :: Flip OR.Array Double 15
-t48 = Flip $ OR.fromList [3, 1, 2, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2] [18.1,29.1,32.1,40.1,52.0,53.99432,97.1,58.8943200001,18.1,29.1,32.1,40.1,58.0,54.99432,97.1,52.8943200001, 5, 2, 6, 1, -2, 0.92, 0.1, -0.2, 13.1, 9, 8, -4, 34, 2.99432, -33, 26, 2, 2, 2, 2, -0.2,-0.2,-0.2,-0.2,25.0003,-0.2,-0.2,-0.2,25.0003,25.0003,25.0003,25.0003]
+t48 :: Flip OR.Array Double 75
+t48 = Flip $ OR.fromList [3, 1, 2, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] [18.1,29.1,32.1,40.1,52.0,53.99432,97.1,58.8943200001,18.1,29.1,32.1,40.1,58.0,54.99432,97.1,52.8943200001, 5, 2, 6, 1, -2, 0.92, 0.1, -0.2, 13.1, 9, 8, -4, 34, 2.99432, -33, 26, 2, 2, 2, 2, -0.2,-0.2,-0.2,-0.2,25.0003,-0.2,-0.2,-0.2,25.0003,25.0003,25.0003,25.0003]
 
 revShort ::
-     (AstRanked PrimalSpan Double 15 -> AstRanked PrimalSpan Double 17)
-     -> Flip OR.Array Double 15
-     -> ADVal (Flip OR.Array) Double 17
+     (AstRanked PrimalSpan Double 75 -> AstRanked PrimalSpan Double 77)
+     -> Flip OR.Array Double 75
+     -> ADVal (Flip OR.Array) Double 77
 revShort f vals =
   let !parameters = toDomains vals in
   let !deltaInputs = traceShow ("parameters", parameters) $ generateDeltaInputsOD parameters in
   let !inputs = traceShow ("deltaInputs", deltaInputs) $ makeADInputs parameters deltaInputs in
   let !(!var, !ast) = traceShow ("inputs", inputs) $ funToAstR (tshape vals) f in
   let !env = traceShow ("var", var) $ traceShow ("ast", ast) $ extendEnvR var (parseDomains vals inputs) EM.empty in
-  let adval :: ADVal (Flip OR.Array) Double 17
+  let adval :: ADVal (Flip OR.Array) Double 77
       !adval = traceShow ("env", env) $ interpretAst env ast
   in adval
 
-concatBuild2 :: AstRanked PrimalSpan Double 15 -> AstRanked PrimalSpan Double 17
+concatBuild2 :: AstRanked PrimalSpan Double 75 -> AstRanked PrimalSpan Double 77
 concatBuild2 r =
   tbuild1 5 (\i ->
     tbuild1 2 (\j -> tmap0N (* tfromIndex0 (maxF j (i `quot` (j + 1)))) r))
