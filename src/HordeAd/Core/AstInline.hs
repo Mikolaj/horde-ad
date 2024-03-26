2bb29a8147774334a44504c2be01afae4c2a7f4e
Mikolaj Konarski <mikolaj@well-typed.com>
2024-03-26 16:59:07 +0100

    Transform Shares into let-bindings as part of the unlet step

--- src/HordeAd/Core/AstInline.hs
+++ src/HordeAd/Core/AstInline.hs
@@ -18,8 +18,11 @@ import qualified Data.EnumMap.Strict as EM
 import qualified Data.EnumSet as ES
 import           Data.List (mapAccumR)
 import           Data.Maybe (fromMaybe)
+import           Data.Proxy (Proxy (Proxy))
+import           Data.Type.Equality (testEquality, (:~:) (Refl))
 import qualified Data.Vector.Generic as V
-import           GHC.TypeLits (KnownNat)
+import           GHC.TypeLits (KnownNat, Nat)
+import           Type.Reflection (typeRep)
 
 import           HordeAd.Core.Ast (AstBool, AstHVector, AstRanked, AstShaped)
 import           HordeAd.Core.Ast hiding
@@ -496,8 +499,14 @@ unletAstHVector6
   :: ADShare -> AstBindings -> AstHVector PrimalSpan
   -> AstHVector PrimalSpan
 unletAstHVector6 l astBindings t =
-  unletAstHVector (emptyUnletEnv l)
-  $ bindsToHVectorLet (bindsToHVectorLet t astBindings) (assocsADShare l)
+  let (memoOut, share) = shareAstHVector EM.empty t
+      bindingsOut = EM.toDescList memoOut
+  in unletAstHVector (emptyUnletEnv l)
+     $ bindsToHVectorLet
+         (bindsToHVectorLet
+           (bindsToHVectorLet share bindingsOut)
+           astBindings)
+         (assocsADShare l)
 
 -- TODO: if a nested let is alone, eliminate the nesting let instead;
 -- this probably requires many passes though
@@ -730,3 +739,315 @@ unletAstBool env t = case t of
     Ast.AstRel opCodeRel (unletAst env arg1) (unletAst env arg2)
   Ast.AstRelS opCodeRel arg1 arg2 ->
     Ast.AstRelS opCodeRel (unletAstS env arg1) (unletAstS env arg2)
+
+
+-- * The pass that removes global sharing, producing a map of shared terms
+
+type ShareMemo = EM.EnumMap AstVarId (AstBindingsCase (AstRanked PrimalSpan))
+
+shareAst
+  :: forall n s r. (GoodScalar r, KnownNat n, AstSpan s)
+  => ShareMemo
+  -> AstRanked s r n -> (ShareMemo, AstRanked s r n)
+shareAst memo v0 = case v0 of
+  Ast.AstVar{} -> (memo, v0)
+  Ast.AstLet var u v ->
+    let (memo1, v2) = shareAst memo v
+        (memo2, u2) = shareAst memo1 u
+    in (memo2, Ast.AstLet var u2 v2)
+  Ast.AstLetADShare{} -> (memo, v0)
+  Ast.AstShare var v | Just Refl <- sameAstSpan @s @PrimalSpan ->
+    -- We assume v is the same if var is the same.
+    let varId = varNameToAstVarId var
+        astVar = Ast.AstVar (shapeAst v) var
+    in if varId `EM.member` memo
+       then (memo, astVar)  -- TODO: memo AstVar
+       else let (memo1, v2) = shareAst memo v
+                d = AstBindingsSimple $ DynamicRanked v2
+            in (EM.insert varId d memo1, astVar)
+  Ast.AstShare{} -> error "shareAst: AstShare not in PrimalSpan"
+  Ast.AstCond b a2 a3 ->
+    let (memo1, b1) = shareAstBool memo b
+        (memo2, t2) = shareAst memo1 a2
+        (memo3, t3) = shareAst memo2 a3
+    in (memo3, Ast.AstCond b1 t2 t3)
+  Ast.AstMinIndex a -> second Ast.AstMinIndex $ shareAst memo a
+  Ast.AstMaxIndex a -> second Ast.AstMaxIndex $ shareAst memo a
+  Ast.AstFloor a -> second Ast.AstFloor $ shareAst memo a
+  Ast.AstIota -> (memo, v0)
+  Ast.AstN1 opCode u ->
+    let (memo2, u2) = shareAst memo u
+    in (memo2, Ast.AstN1 opCode u2)
+  Ast.AstN2 opCode u v ->
+    let (memo2, u2) = shareAst memo u
+        (memo3, v3) = shareAst memo2 v
+    in (memo3, Ast.AstN2 opCode u2 v3)
+  Ast.AstR1 opCode u ->
+    let (memo2, u2) = shareAst memo u
+    in (memo2, Ast.AstR1 opCode u2)
+  Ast.AstR2 opCode u v ->
+    let (memo2, u2) = shareAst memo u
+        (memo3, v3) = shareAst memo2 v
+    in (memo3, Ast.AstR2 opCode u2 v3)
+  Ast.AstI2 opCode u v ->
+    let (memo2, u2) = shareAst memo u
+        (memo3, v3) = shareAst memo2 v
+    in (memo3, Ast.AstI2 opCode u2 v3)
+  Ast.AstSumOfList args ->
+    let (memo2, args2) = mapAccumR shareAst memo args
+    in (memo2, Ast.AstSumOfList args2)
+  Ast.AstIndex v ix ->
+    let (memo1, v2) = shareAst memo v
+        (memo2, ix2) = mapAccumR shareAst memo1 (indexToList ix)
+    in (memo2, Ast.AstIndex v2 (listToIndex ix2))
+  Ast.AstSum v -> second Ast.AstSum (shareAst memo v)
+  Ast.AstScatter sh v (vars, ix) ->
+    let (memo1, v2) = shareAst memo v
+        (memo2, ix2) = mapAccumR shareAst memo1 (indexToList ix)
+    in (memo2, Ast.AstScatter sh v2 (vars, listToIndex ix2))
+  Ast.AstFromList l ->
+    let (memo2, l2) = mapAccumR shareAst memo l
+    in (memo2, Ast.AstFromList l2)
+  Ast.AstFromVector l ->
+    let (memo2, l2) = mapAccumR shareAst memo (V.toList l)
+    in (memo2, Ast.AstFromVector $ V.fromList l2)
+  Ast.AstReplicate k v -> second (Ast.AstReplicate k) (shareAst memo v)
+  Ast.AstAppend x y ->
+    let (memo1, t1) = shareAst memo x
+        (memo2, t2) = shareAst memo1 y
+    in (memo2, Ast.AstAppend t1 t2)
+  Ast.AstSlice i k v -> second (Ast.AstSlice i k) (shareAst memo v)
+  Ast.AstReverse v -> second Ast.AstReverse (shareAst memo v)
+  Ast.AstTranspose perm v ->
+    second (Ast.AstTranspose perm) $ shareAst memo v
+  Ast.AstReshape sh v -> second (Ast.AstReshape sh) (shareAst memo v)
+  Ast.AstBuild1 k (var, v) ->
+    let (memo1, v2) = shareAst memo v
+    in (memo1, Ast.AstBuild1 k (var, v2))
+  Ast.AstGather sh v (vars, ix) ->
+    let (memo1, v2) = shareAst memo v
+        (memo2, ix2) = mapAccumR shareAst memo1 (indexToList ix)
+    in (memo2, Ast.AstGather sh v2 (vars, listToIndex ix2))
+  Ast.AstCast v -> second Ast.AstCast $ shareAst memo v
+  Ast.AstFromIntegral v -> second Ast.AstFromIntegral $ shareAst memo v
+  Ast.AstConst{} -> (memo, v0)
+  Ast.AstLetHVectorIn vars l v ->
+    let (memo1, l2) = shareAstHVector memo l
+        (memo2, v2) = shareAst memo1 v
+    in (memo2, Ast.AstLetHVectorIn vars l2 v2)
+  Ast.AstLetHFunIn var f v ->
+    let (memo1, v2) = shareAst memo v
+        (memo2, f2) = shareAstHFun memo1 f
+    in (memo2, Ast.AstLetHFunIn var f2 v2)
+  Ast.AstRFromS v -> second Ast.AstRFromS $ shareAstS memo v
+  Ast.AstConstant a -> second Ast.AstConstant $ shareAst memo a
+  Ast.AstPrimalPart a -> second Ast.AstPrimalPart $ shareAst memo a
+  Ast.AstDualPart a -> second Ast.AstDualPart $ shareAst memo a
+  Ast.AstD u u' ->
+    let (memo1, t1) = shareAst memo u
+        (memo2, t2) = shareAst memo1 u'
+    in (memo2, Ast.AstD t1 t2)
+
+shareAstS
+  :: forall sh s r. (GoodScalar r, Sh.Shape sh, AstSpan s)
+  => ShareMemo
+  -> AstShaped s r sh -> (ShareMemo, AstShaped s r sh)
+shareAstS memo v0 = case v0 of
+  Ast.AstVarS{} -> (memo, v0)
+  Ast.AstLetS var u v ->
+    let (memo1, v2) = shareAstS memo v
+        (memo2, u2) = shareAstS memo1 u
+    in (memo2, Ast.AstLetS var u2 v2)
+  Ast.AstLetADShareS{} -> (memo, v0)
+  Ast.AstShareS var v | Just Refl <- sameAstSpan @s @PrimalSpan ->
+    -- We assume v is the same if var is the same.
+    let varId = varNameToAstVarId var
+        astVar = Ast.AstVarS var
+    in if varId `EM.member` memo
+       then (memo, astVar)
+       else let (memo1, v2) = shareAstS memo v
+                d = AstBindingsSimple $ DynamicShaped v2
+            in (EM.insert varId d memo1, astVar)
+  Ast.AstShareS{} -> error "shareAstS: AstShareS not in PrimalSpan"
+  Ast.AstCondS b a2 a3 ->
+    let (memo1, b1) = shareAstBool memo b
+        (memo2, t2) = shareAstS memo1 a2
+        (memo3, t3) = shareAstS memo2 a3
+    in (memo3, Ast.AstCondS b1 t2 t3)
+  Ast.AstMinIndexS a -> second Ast.AstMinIndexS $ shareAstS memo a
+  Ast.AstMaxIndexS a -> second Ast.AstMaxIndexS $ shareAstS memo a
+  Ast.AstFloorS a -> second Ast.AstFloorS $ shareAstS memo a
+  Ast.AstIotaS -> (memo, v0)
+  Ast.AstN1S opCode u ->
+    let (memo2, u2) = shareAstS memo u
+    in (memo2, Ast.AstN1S opCode u2)
+  Ast.AstN2S opCode u v ->
+    let (memo2, u2) = shareAstS memo u
+        (memo3, v3) = shareAstS memo2 v
+    in (memo3, Ast.AstN2S opCode u2 v3)
+  Ast.AstR1S opCode u ->
+    let (memo2, u2) = shareAstS memo u
+    in (memo2, Ast.AstR1S opCode u2)
+  Ast.AstR2S opCode u v ->
+    let (memo2, u2) = shareAstS memo u
+        (memo3, v3) = shareAstS memo2 v
+    in (memo3, Ast.AstR2S opCode u2 v3)
+  Ast.AstI2S opCode u v ->
+    let (memo2, u2) = shareAstS memo u
+        (memo3, v3) = shareAstS memo2 v
+    in (memo3, Ast.AstI2S opCode u2 v3)
+  Ast.AstSumOfListS args ->
+    let (memo2, args2) = mapAccumR shareAstS memo args
+    in (memo2, Ast.AstSumOfListS args2)
+  Ast.AstIndexS @sh1 v ix ->
+    let (memo1, v2) = shareAstS memo v
+        (memo2, ix2) = mapAccumR shareAst memo1 (ShapedList.indexToList ix)
+    in (memo2, Ast.AstIndexS @sh1 v2 (ShapedList.listToIndex ix2))
+  Ast.AstSumS v -> second Ast.AstSumS (shareAstS memo v)
+  Ast.AstScatterS @sh2 @p v (vars, ix) ->
+    let (memo1, v2) = shareAstS memo v
+        (memo2, ix2) = mapAccumR shareAst memo1 (ShapedList.indexToList ix)
+    in (memo2, Ast.AstScatterS @sh2 @p v2 (vars, ShapedList.listToIndex ix2))
+  Ast.AstFromListS l ->
+    let (memo2, l2) = mapAccumR shareAstS memo l
+    in (memo2, Ast.AstFromListS l2)
+  Ast.AstFromVectorS l ->
+    let (memo2, l2) = mapAccumR shareAstS memo (V.toList l)
+    in (memo2, Ast.AstFromVectorS $ V.fromList l2)
+  Ast.AstReplicateS v -> second Ast.AstReplicateS (shareAstS memo v)
+  Ast.AstAppendS x y ->
+    let (memo1, t1) = shareAstS memo x
+        (memo2, t2) = shareAstS memo1 y
+    in (memo2, Ast.AstAppendS t1 t2)
+  Ast.AstSliceS @i v -> second (Ast.AstSliceS @i) (shareAstS memo v)
+  Ast.AstReverseS v -> second Ast.AstReverseS (shareAstS memo v)
+  Ast.AstTransposeS @perm v ->
+    second (Ast.AstTransposeS @perm) $ shareAstS memo v
+  Ast.AstReshapeS v -> second Ast.AstReshapeS (shareAstS memo v)
+  Ast.AstBuild1S @n (var, v) ->
+    let (memo1, v2) = shareAstS memo v
+    in (memo1, Ast.AstBuild1S (var, v2))
+  Ast.AstGatherS @sh2 @p v (vars, ix) ->
+    let (memo1, v2) = shareAstS memo v
+        (memo2, ix2) = mapAccumR shareAst memo1 (ShapedList.indexToList ix)
+    in (memo2, Ast.AstGatherS @sh2 @p v2 (vars, ShapedList.listToIndex ix2))
+  Ast.AstCastS v -> second Ast.AstCastS $ shareAstS memo v
+  Ast.AstFromIntegralS v ->
+    second Ast.AstFromIntegralS $ shareAstS memo v
+  Ast.AstConstS{} -> (memo, v0)
+  Ast.AstLetHVectorInS vars l v ->
+    let (memo1, l2) = shareAstHVector memo l
+        (memo2, v2) = shareAstS memo1 v
+    in (memo2, Ast.AstLetHVectorInS vars l2 v2)
+  Ast.AstLetHFunInS var f v ->
+    let (memo1, v2) = shareAstS memo v
+        (memo2, f2) = shareAstHFun memo1 f
+    in (memo2, Ast.AstLetHFunInS var f2 v2)
+  Ast.AstSFromR v -> second Ast.AstSFromR $ shareAst memo v
+  Ast.AstConstantS a -> second Ast.AstConstantS $ shareAstS memo a
+  Ast.AstPrimalPartS a -> second Ast.AstPrimalPartS $ shareAstS memo a
+  Ast.AstDualPartS a -> second Ast.AstDualPartS $ shareAstS memo a
+  Ast.AstDS u u' ->
+    let (memo1, t1) = shareAstS memo u
+        (memo2, t2) = shareAstS memo1 u'
+    in (memo2, Ast.AstDS t1 t2)
+
+shareAstDynamic
+  :: AstSpan s
+  => ShareMemo -> AstDynamic s -> (ShareMemo, AstDynamic s)
+shareAstDynamic memo = \case
+  DynamicRanked w -> second DynamicRanked $ shareAst memo w
+  DynamicShaped w -> second DynamicShaped $ shareAstS memo w
+  u@DynamicRankedDummy{} -> (memo, u)
+  u@DynamicShapedDummy{} -> (memo, u)
+
+shareAstHVector
+  :: forall s. AstSpan s
+  => ShareMemo -> AstHVector s -> (ShareMemo, AstHVector s)
+shareAstHVector memo v0 = case v0 of
+  Ast.AstMkHVector l ->
+    second Ast.AstMkHVector $ mapAccumR shareAstDynamic memo l
+  Ast.AstHApply t ll ->
+    let (memo1, t2) = shareAstHFun memo t
+        (memo2, ll2) = mapAccumR (mapAccumR shareAstDynamic) memo1 ll
+    in (memo2, Ast.AstHApply t2 ll2)
+  Ast.AstLetHVectorInHVector vars u v ->
+    let (memo1, u2) = shareAstHVector memo u
+        (memo2, v2) = shareAstHVector memo1 v
+    in (memo2, Ast.AstLetHVectorInHVector vars u2 v2)
+  Ast.AstLetHFunInHVector var f v ->
+    let (memo1, v2) = shareAstHVector memo v
+        (memo2, f2) = shareAstHFun memo1 f
+    in (memo2, Ast.AstLetHFunInHVector var f2 v2)
+  Ast.AstLetInHVector var u v ->
+    let (memo1, v2) = shareAstHVector memo v
+        (memo2, u2) = shareAst memo1 u
+    in (memo2, Ast.AstLetInHVector var u2 v2)
+  Ast.AstLetInHVectorS var u v ->
+    let (memo1, v2) = shareAstHVector memo v
+        (memo2, u2) = shareAstS memo1 u
+    in (memo2, Ast.AstLetInHVectorS var u2 v2)
+  Ast.AstShareHVector vars l | Just Refl <- sameAstSpan @s @PrimalSpan ->
+    -- We assume l is the same if vars are the same.
+    let var = vars !! 0  -- vars are fresh, so var uniquely represent vars
+        varId = dynamicVarNameToAstVarId var
+        f :: AstDynamicVarName -> DynamicTensor (AstRanked PrimalSpan)
+        f (AstDynamicVarName @ty @rD @shD varIdD) =
+          case testEquality (typeRep @ty) (typeRep @Nat) of
+            Just Refl -> withListSh (Proxy @shD) $ \sh ->
+              DynamicRanked @rD $ Ast.AstVar sh (AstVarName varIdD)
+            _ -> DynamicShaped @rD @shD $ Ast.AstVarS (AstVarName varIdD)
+        astVars = Ast.AstMkHVector $ V.fromList $ map f vars
+    in if varId `EM.member` memo
+       then (memo, astVars)
+       else let (memo1, l2) = shareAstHVector memo l
+                d = AstBindingsHVector vars l2
+            in (EM.insert varId d memo1, astVars)
+  Ast.AstShareHVector{} ->
+    error "shareAstHVector: AstShareHVector not in PrimalSpan"
+  Ast.AstBuildHVector1 k (var, v) ->
+    let (memo1, v2) = shareAstHVector memo v
+    in (memo1, Ast.AstBuildHVector1 k (var, v2))
+  Ast.AstMapAccumRDer k accShs bShs eShs f df rf acc0 es ->
+    let (memo1, f2) = shareAstHFun memo f
+        (memo2, df2) = shareAstHFun memo1 df
+        (memo3, rf2) = shareAstHFun memo2 rf
+        (memo4, acc02) = shareAstHVector memo3 acc0
+        (memo5, es2) = shareAstHVector memo4 es
+    in (memo5, Ast.AstMapAccumRDer k accShs bShs eShs f2 df2 rf2 acc02 es2)
+  Ast.AstMapAccumLDer k accShs bShs eShs f df rf acc0 es ->
+    let (memo1, f2) = shareAstHFun memo f
+        (memo2, df2) = shareAstHFun memo1 df
+        (memo3, rf2) = shareAstHFun memo2 rf
+        (memo4, acc02) = shareAstHVector memo3 acc0
+        (memo5, es2) = shareAstHVector memo4 es
+    in (memo5, Ast.AstMapAccumLDer k accShs bShs eShs f2 df2 rf2 acc02 es2)
+
+shareAstHFun
+  :: ShareMemo -> AstHFun -> (ShareMemo, AstHFun)
+shareAstHFun memo v0 = case v0 of
+  Ast.AstLambda{} ->
+    -- No other free variables in l, so no outside lets can reach there,
+    -- so we don't need to pass the information from v upwards
+    -- nor remove the Share constructors.
+    (memo, v0)
+  Ast.AstVarHFun{} -> (memo, v0)
+
+shareAstBool :: ShareMemo -> AstBool -> (ShareMemo, AstBool)
+shareAstBool memo v0 = case v0 of
+  Ast.AstBoolNot arg ->
+    let (memo2, arg2) = shareAstBool memo arg
+    in (memo2, Ast.AstBoolNot arg2)
+  Ast.AstB2 opCodeBool arg1 arg2 ->
+    let (memo1, b1) = shareAstBool memo arg1
+        (memo2, b2) = shareAstBool memo1 arg2
+    in (memo2, Ast.AstB2 opCodeBool b1 b2)
+  Ast.AstBoolConst{} -> (memo, v0)
+  Ast.AstRel opCodeRel arg1 arg2 ->
+    let (memo1, r1) = shareAst memo arg1
+        (memo2, r2) = shareAst memo1 arg2
+    in (memo2, Ast.AstRel opCodeRel r1 r2)
+  Ast.AstRelS opCodeRel arg1 arg2 ->
+    let (memo1, r1) = shareAstS memo arg1
+        (memo2, r2) = shareAstS memo1 arg2
+    in (memo2, Ast.AstRelS opCodeRel r1 r2)
