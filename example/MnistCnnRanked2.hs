53e3cd9d96934e68cb82816f8b92783f6122b2a6
Mikolaj Konarski <mikolaj@well-typed.com>
2024-11-15 19:24:28 +0100

    Minimizing yet more

--- example/MnistCnnRanked2.hs
+++ example/MnistCnnRanked2.hs
@@ -18,18 +18,3 @@ import HordeAd.Internal.BackendOX (RepN (..))
 import HordeAd.Internal.OrthotopeOrphanInstances (FlipR (..))
 import HordeAd.Util.SizedList
 import MnistData
-
-type ADCnnMnistParametersShaped (target :: Target) r =
-  target (TKS r '[1, 1, 2, 2])
-
-type ADCnnMnistParameters (target :: Target) r =
-  target (TKR r 4)
-
-convMnistTwoR
-  :: (ADReady target, GoodScalar r)
-  => PrimalOf target (TKR r 4)  -- [batch_size, 1, SizeMnistHeight, SizeMnistWidth]
-  -> ADCnnMnistParameters target r
-  -> target (TKR r 2)  -- [SizeMnistLabel, batch_size]
-convMnistTwoR input ker1 =
-  let t1 = maxPool2dUnpadded 2 2 $ conv2dUnpadded ker1 (rfromPrimal input)
-  in rreshape (1 :$: 1 :$: ZSR) t1
--- test/simplified/TestMnistCNNR.hs
+++ test/simplified/TestMnistCNNR.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedLists #-}
 module TestMnistCNNR
   ( testTrees
   ) where
@@ -7,7 +8,7 @@ import Prelude
 import Control.Monad (foldM, unless)
 import Data.IntMap.Strict qualified as IM
 import Data.Vector.Generic qualified as V
-import GHC.TypeLits (SomeNat (..), someNatVal)
+import GHC.TypeLits (KnownNat, SomeNat (..), someNatVal)
 import Numeric.LinearAlgebra (Numeric)
 import System.IO (hPutStrLn, stderr)
 import System.Random
@@ -32,33 +33,54 @@ import MnistCnnRanked2 qualified
 import MnistData
 
 testTrees :: [TestTree]
-testTrees = [ tensorMnistTestsPP
-            ]
-
-tensorMnistTestsPP :: TestTree
-tensorMnistTestsPP = testGroup "PP tests for CNN MNIST tests"
-  [ testCase "CNNOPP" testCNNOPP
-  ]
+testTrees = [testCase "CNNOPP" testCNNOPP]
 
 testCNNOPP :: Assertion
 testCNNOPP = do
-  resetVarCounter
-  let blackGlyph :: AstTensor AstMethodLet PrimalSpan (TKR Double 4)
-      blackGlyph = AstReplicate (SNat @1)
+  let blackGlyph :: AstTensor AstMethodLet FullSpan (TKR Double 4)
+      blackGlyph = AstFromPrimal $ AstReplicate (SNat @1)
                    $ AstReplicate (SNat @1)
                    $ AstReplicate (SNat @2)
                    $ AstReplicate (SNat @2)
                        (AstConcrete (Nested.rscalar 7) :: AstTensor AstMethodLet PrimalSpan (TKR Double 0))
-      valsInit :: MnistCnnRanked2.ADCnnMnistParameters RepN Double
-      valsInit =
-        forgetShape $ fst
-        $ randomVals @(MnistCnnRanked2.ADCnnMnistParametersShaped
-                         RepN Double)
-                     0.4 (mkStdGen 44)
-      afcnn2T :: MnistCnnRanked2.ADCnnMnistParameters (AstTensor AstMethodLet FullSpan)
-                                                      Double
-              -> AstTensor AstMethodLet FullSpan (TKR Double 2)
-      afcnn2T = MnistCnnRanked2.convMnistTwoR blackGlyph
-      (artifactRev, _) = revArtifactAdapt True afcnn2T valsInit
-  printArtifactPretty IM.empty artifactRev
+      afcnn2T :: AstTensor AstMethodLet FullSpan (TKR Double 4)
+      afcnn2T = maxPool2dUnpadded2 $ conv2dUnpadded2 blackGlyph
+  printAstPretty IM.empty afcnn2T
     @?= ""
+
+maxPool2dUnpadded2
+  :: (ADReady target, GoodScalar r)
+  => target (TKR r 4) -> target (TKR r 4)
+maxPool2dUnpadded2 arr =
+  rbuild [1, 1, 1, 1] $ \case
+    [_, _, iBh, iBw] ->
+      let arrt = slicez2 [1, 1, 2, 2] arr [1, 1, 2 * iBh, 2 * iBw]
+      in rmaximum2 arrt
+    _ -> error "maxPool2dUnpadded: impossible pattern needlessly required"
+
+conv2dUnpadded2
+  :: (ADReady target, GoodScalar r)
+  => target (TKR r 4) -> target (TKR r 4)
+conv2dUnpadded2 arrA =
+  let shB = [1, 1, 2, 2]
+  in rbuild shB $ \case
+    [iImg, _, iBh, iBw] ->
+      let arrAt = slicez2 shB arrA [iImg, 0, iBh, iBw]
+      in rindex0 arrAt [0, 0, 0, 0]
+    _ -> error "conv2dUnpadded: impossible pattern needlessly required"
+
+slicez2
+  :: (ADReady target, GoodScalar r, KnownNat n)
+  => IShR n -> target (TKR r n) -> IndexOf target n -> target (TKR r n)
+slicez2 shOut d ixBase =
+  rbuild shOut $ \ixResult -> indexz02 d (zipWith_Index (+) ixBase ixResult)
+
+indexz02
+  :: forall target r n. (ADReady target, GoodScalar r, KnownNat n)
+  => target (TKR r n) -> IndexOf target n -> target (TKR r 0)
+indexz02 d ix = ifF (within0 @target (rshape @target d) ix) (d ! ix) (rscalar 0)
+
+rmaximum2 :: ( BaseTensor target, BaseTensor (PrimalOf target)
+            , LetTensor target, KnownNat n, GoodScalar r )
+         => target (TKR r n) -> target (TKR r 0)
+rmaximum2 t0 = tlet t0 $ \t -> rindex0 t [0, 0, 0, 0]
