6afe8fe14f60c2d35fc4b2a6855f7f73c4234794
Mikolaj Konarski <mikolaj@well-typed.com>
2024-11-15 17:18:47 +0100

    Minimize some more

--- test/simplified/TestMnistFCNNR.hs
+++ test/simplified/TestMnistFCNNR.hs
@@ -2,125 +2,97 @@ module TestMnistFCNNR ( testTrees ) where
 
 import Prelude
 
-import           Control.Monad (foldM)
 import qualified Data.Array.DynamicS as OD
 import qualified Data.Array.RankedS as OR
 import           Data.Bifunctor.Flip
-import qualified Data.EnumMap.Strict as EM
-import qualified Data.Vector.Generic as V
 import           GHC.TypeLits (Nat, SomeNat (..), someNatVal)
 
-import           HordeAd
 import           HordeAd.Core.Adaptor
-import           HordeAd.Core.AstEnv
-import           HordeAd.Core.AstFreshId (funToAstIOR)
+import           HordeAd.Core.Ast
+import           HordeAd.Core.AstInterpret
+import           HordeAd.Core.DualNumber
+import           HordeAd.Core.Engine
 import           HordeAd.Core.TensorADVal (ADValClown)
-import           HordeAd.External.OptimizerTools
-import           MnistData
+import           HordeAd.Core.Types
+import           HordeAd.External.CommonRankedOps
 import qualified MnistFcnnRanked2
 
 testTrees :: [IO ()]
 testTrees = [ mnistTestCase2VTA
             , mnistTestCase2VTA
-            , mnistTestCase2VTI 2 5 500
-                       (0.884 :: Float)
-            , mnistTestCase2VTI 4 1 499
-                       (0.7464929859719439 :: Double)
-            , mnistTestCase2VTI 0 0.02 500
-                       (1 :: Float)
-            , mnistTestCase2VTO 4 2 1 499
-            , mnistTestCase2VTO 0 100 0.02 500
+            , mnistTestCase2VTI
+            , mnistTestCase2VTI
+            , mnistTestCase2VTO
             ]
+
+{- moving any of these to this file removes the crash:
+afcnnMnistLoss2
+  :: (ADReady ranked, GoodScalar r, Differentiable r)
+  => MnistData r -> MnistFcnnRanked2.ADFcnnMnist2Parameters ranked r
+  -> ranked r 0
+afcnnMnistLoss2 (datum, target) =
+  let datum1 = tconst $ OR.fromVector [sizeMnistGlyphInt] datum
+      target1 = tconst $ OR.fromVector [sizeMnistLabelInt] target
+  in MnistFcnnRanked2.afcnnMnistLoss2TensorData (datum1, target1)
+
+afcnnMnistTest2
+  :: forall ranked r.
+     (ranked ~ Flip OR.Array, GoodScalar r, Differentiable r)
+  => MnistFcnnRanked2.ADFcnnMnist2Parameters ranked r
+  -> [MnistData r]
+  -> DomainsOD
+  -> r
+afcnnMnistTest2 _ [] _ = 0
+afcnnMnistTest2 valsInit dataList testParams =
+  let matchesLabels :: MnistData r -> Bool
+      matchesLabels (glyph, label) =
+        let glyph1 = tconst $ OR.fromVector [sizeMnistGlyphInt] glyph
+            nn :: MnistFcnnRanked2.ADFcnnMnist2Parameters ranked r
+               -> ranked r 1
+            nn = inline MnistFcnnRanked2.afcnnMnist2 logistic softMax1 glyph1
+            v = OR.toVector $ runFlip $ nn $ parseDomains valsInit testParams
+        in V.maxIndex v == V.maxIndex label
+  in fromIntegral (length (filter matchesLabels dataList))
+     / fromIntegral (length dataList)
+-}
+
+
+-- | Stochastic Gradient Descent.
+sgd :: forall n r a. GoodScalar r
+    => (a -> Domains (ADValClown OD.Array) -> ADVal (Flip OR.Array) r n)
+    -> (DomainsOD, Flip OR.Array r n)
+sgd f = undefined
 
 mnistTestCase2VTA :: forall ranked. IO ()
 mnistTestCase2VTA = do
-  let valsInit :: MnistFcnnRanked2.ADFcnnMnist2Parameters (Flip OR.Array) Double
-      valsInit = undefined
-      runBatch :: DomainsOD -> [MnistData Double] -> IO DomainsOD
-      runBatch _ chunk = do
-        let f :: MnistData Double -> Domains (ADValClown OD.Array)
-              -> ADVal (Flip OR.Array) Double 0
-            f _ adinputs =
-              MnistFcnnRanked2.afcnnMnistLoss2
-                undefined (parseDomains valsInit adinputs)
-        return $ fst $ sgd undefined f chunk undefined
-      chunks = chunksOf undefined undefined
-  res <- foldM runBatch undefined chunks
+  let f :: a -> Domains (ADValClown OD.Array)
+        -> ADVal (Flip OR.Array) Double 0
+      f _ adinputs =
+        MnistFcnnRanked2.afcnnMnistLoss2
+          undefined (parseDomains undefined adinputs)
+      !dd = sgd f
   return ()
 
 mnistTestCase2VTI
-  :: forall ranked r. ranked ~ Flip OR.Array
-  => Int -> Double -> Int -> r -> IO ()
-mnistTestCase2VTI maxBatches gamma batchSize _ = do
-  (varGlyph, _, _) <-
-    funToAstIOR (singletonShape sizeMnistGlyphInt) id
-  (varLabel, _, _) <-
-    funToAstIOR (singletonShape sizeMnistLabelInt) id
-  let ast :: AstRanked PrimalSpan Double 0
-      ast = undefined
-  let runBatch :: DomainsOD -> [MnistData Double] -> IO DomainsOD
-      runBatch !domains chunk = do
-        let f :: MnistData Double -> Domains (ADValClown OD.Array)
-              -> ADVal ranked Double 0
-            f (glyph, label) varInputs =
-              let env = foldr extendEnvDR EM.empty
-                        $ zip undefined $ V.toList varInputs
-                  envMnist =
-                    extendEnvR varGlyph
-                      (tconst $ OR.fromVector [sizeMnistGlyphInt] glyph)
-                    $ extendEnvR varLabel
-                        (tconst $ OR.fromVector [sizeMnistLabelInt] label)
-                        env
-              in interpretAst envMnist ast
-            res = fst $ sgd gamma f chunk domains
-        return res
-      chunks = take maxBatches $ chunksOf batchSize undefined
-  res <- foldM runBatch undefined chunks
+  :: forall ranked. ranked ~ Flip OR.Array
+  => IO ()
+mnistTestCase2VTI = do
+  let f :: a -> b -> ADVal ranked Double 0
+      f _ _ = interpretAst undefined (undefined :: AstRanked PrimalSpan Double 0)
+      !dd = sgd f
   return ()
 
-mnistTestCase2VTO
-  :: Int -> Int -> Double -> Int -> IO ()
-mnistTestCase2VTO maxBatches widthHidden2 gamma batchSize =
- case someNatVal $ toInteger widthHidden2 of
-   Nothing -> error "impossible someNatVal error"
+mnistTestCase2VTO :: IO ()
+mnistTestCase2VTO =
+ case someNatVal 3 of
    Just (SomeNat _) -> do
-    let valsInit :: MnistFcnnRanked2.ADFcnnMnist2Parameters (Flip OR.Array) Double
-        valsInit = undefined
-        ftest :: [MnistData Double] -> DomainsOD -> Double
-        ftest = MnistFcnnRanked2.afcnnMnistTest2 undefined
-    (varGlyph, varGlyphD, astGlyph) <-
-      funToAstIOR (singletonShape sizeMnistGlyphInt) id
-    (varLabel, varLabelD, astLabel) <-
-      funToAstIOR (singletonShape sizeMnistLabelInt) id
-    let envInit = extendEnvR varGlyph (tconstant astGlyph)
-                  $ extendEnvR varLabel (tconstant astLabel)
-                    EM.empty
-        f = MnistFcnnRanked2.afcnnMnistLoss2TensorData @(AstRanked FullSpan)
-              (tconstant astGlyph, tconstant astLabel)
-        g domains = f $ parseDomains valsInit domains
-        (((varDtAgain, vars1Again), gradientRaw, primal, sh), _) =
-          revProduceArtifact False g envInit undefined
-        gradient = simplifyAstDomains6 gradientRaw
-        vars1AndInputAgain = vars1Again ++ [varGlyphD, varLabelD]
-        vars = (varDtAgain, vars1AndInputAgain)
-        go :: [MnistData Double] -> DomainsOD -> DomainsOD
-        go [] parameters = parameters
-        go ((glyph, label) : rest) !parameters =
-          let glyphD = DynamicExists
-                       $ OD.fromVector [sizeMnistGlyphInt] glyph
-              labelD = DynamicExists
-                       $ OD.fromVector [sizeMnistLabelInt] label
-              parametersAndInput =
-                V.concat [parameters, V.fromList [glyphD, labelD]]
-              gradientDomain =
-                fst $ revEvalArtifact @Nat @AstRanked
-                                      (vars, gradient, primal, sh)
-                                      parametersAndInput Nothing
-          in go rest (updateWithGradient gamma parameters gradientDomain)
-    let runBatch :: DomainsOD -> [MnistData Double] -> IO DomainsOD
-        runBatch !domains chunk =
-          return $ go chunk domains
-        chunks = take maxBatches $ chunksOf batchSize undefined
-    res <- foldM runBatch undefined chunks
-    let !testErrorFinal = ftest undefined res
+    let !result = MnistFcnnRanked2.afcnnMnist2 logistic undefined undefined (undefined :: MnistFcnnRanked2.ADFcnnMnist2Parameters (AstRanked FullSpan) Double)
+        g domains = (undefined (parseDomains undefined domains :: MnistFcnnRanked2.ADFcnnMnist2Parameters (AstRanked FullSpan) Double) :: AstRanked FullSpan Double 0)
+        ((vars, _, _, _), _) = revProduceArtifact undefined g undefined undefined
+        !gradientDomain =
+          revEvalArtifact @Nat @AstRanked
+                          (vars, undefined, undefined, undefined)
+                          undefined Nothing
+        ftest :: DomainsOD -> Double
+        !ftest = MnistFcnnRanked2.afcnnMnistTest2 undefined undefined
     return ()
