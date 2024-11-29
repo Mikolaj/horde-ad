281414779864b5676384db4e3e116eb43ba62088
Mikolaj Konarski <mikolaj@well-typed.com>
2024-11-29 20:48:49 +0100

    Pretty-print rfromScalar and rtoScalar

--- src/HordeAd/Core/AstPrettyPrint.hs
+++ src/HordeAd/Core/AstPrettyPrint.hs
@@ -151,8 +151,8 @@ printAst cfgOld d t =
 printAstAux :: forall s y ms. (TensorKind y, AstSpan s)
             => PrintConfig -> Int -> AstTensor ms s y -> ShowS
 printAstAux cfg d = \case
-  AstFromScalar t -> printAstAux cfg d t  -- TODO
-  AstToScalar t -> printAstAux cfg d t  -- TODO
+  AstFromScalar t -> printPrefixOp printAst cfg d "rfromScalar" [t]
+  AstToScalar t -> printPrefixOp printAst cfg d "rtoScalar" [t]
   AstPair t1 t2 ->
     showParen (d > 10)
     $ showString "tpair ("  -- TODO
