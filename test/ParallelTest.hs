aca2185708f242a1b4037ca575b34e9aa3780c5a
Mikolaj Konarski <mikolaj@well-typed.com>
2025-03-06 11:26:50 +0100

    Add a parallel test

---
+++ test/ParallelTest.hs
@@ -0,0 +1,36 @@
+module Main (main) where
+
+import Prelude
+
+import Data.Proxy
+import System.IO qualified as SIO
+import Test.Tasty
+import Test.Tasty.Options
+import Test.Tasty.Runners
+
+import EqEpsilon
+import TestMnistCNNR qualified
+import TestMnistFCNNR qualified
+import TestMnistRNNR qualified
+import TestMnistRNNS qualified
+
+main :: IO ()
+main = do
+  -- Limit interleaving of characters in parallel tests.
+  SIO.hSetBuffering SIO.stdout SIO.LineBuffering
+  SIO.hSetBuffering SIO.stderr SIO.LineBuffering
+  opts <- parseOptions (ingredients : defaultIngredients) tests
+  setEpsilonEq (lookupOption opts :: EqEpsilon)
+  defaultMainWithIngredients (ingredients : defaultIngredients) tests
+ where
+  ingredients = includingOptions [Option (Proxy :: Proxy EqEpsilon)]
+
+tests :: TestTree
+tests =
+  testGroup "The set of tests for simplified horde-ad that can run in parallel"
+    [ testGroup "Long_tests"
+        (TestMnistCNNR.testTrees
+         ++ TestMnistFCNNR.testTrees
+         ++ TestMnistRNNR.testTrees
+         ++ TestMnistRNNS.testTrees)
+    ]
