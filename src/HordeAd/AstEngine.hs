ed07b05123e92564e2f485ef8ac51d1a4223e8aa
Mikolaj Konarski <mikolaj@well-typed.com>
2025-03-29 17:56:37 +0100

    Work around the lack of idempotence of contractAst

--- src/HordeAd/AstEngine.hs
+++ src/HordeAd/AstEngine.hs
@@ -64,7 +64,10 @@ simplifyInlineContract
   :: forall z s. AstSpan s
   => AstTensor AstMethodLet s z -> AstTensor AstMethodLet s z
 simplifyInlineContract =
-  contractAst . simplifyInline
+  contractAst  -- TODO: this is needed, but shoult not be; fix in AstSimplify;
+               -- generally, contractAst and all the other should be
+               -- idempotent up to renaming, or close
+  . contractAst . simplifyInline
 
 
 -- * Pretty-printing terms in a few useful configurations
